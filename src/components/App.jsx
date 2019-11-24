1c602acf93c50497fefc08ca6e4acd1467b63f9b
deepak bhatt <deepakbhattmits@gmail.com>
2019-11-24 21:14:10 +0530

    using new module with datepicker

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamList from './streams/StreamList';
@@ -6,8 +6,24 @@ import StreamShow from './streams/StreamShow';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import Header from './Header';
+import GridLayout from 'react-grid-layout';
+import { ReactComponent as CalenderSVG } from '../assets/images/icon-calendar.svg'
+import DatePicker from "react-datepicker";
 import createBrowserHistory from '../history';
 const App = () => {
+    const layout = [
+        { i: 'a', x: 0, y: 0, w: 4, h: 2, minW: 2, maxW: 4 },
+        { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+        { i: 'c', x: 4, y: 0, w: 1, h: 2 }
+    ];
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const datePicker = useRef();
+    const onChange = value => {
+        setSelectedDate(value)
+    }
+    const onChangeSVG = () => {
+        datePicker.current.handleFocus();
+    }
     return (
         <div className="ui container">
             <Router history={createBrowserHistory}>
@@ -20,6 +36,30 @@ const App = () => {
                         <Route path="/streams/delete/:id" exact component={StreamDelete}></Route>
                         <Route path="/streams/show/:id" exact component={StreamShow}></Route>
                     </Switch>
+                </div>
+                <div className="grid">
+                    <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+                        <div key="a">a
+                            <div className='datepicker'>
+                                <DatePicker
+                                    ref={datePicker}
+                                    onChange={onChange}
+                                    selected={selectedDate} // display the current date  
+                                    // placeholderText="DD-MM-YYYY" // displayed only when selected is not there 
+                                    dateFormat="dd-MM-yyyy" // if you want to make some changes with format then use this attribute by default value displayed like MM/DD/YYYY
+
+                                />
+                                <CalenderSVG
+                                    className='icon icon--calender'
+                                    onClick={onChangeSVG}
+                                />
+                            </div>
+
+
+                        </div>
+                        <div key="b">b</div>
+                        <div key="c">c</div>
+                    </GridLayout>
                 </div>
             </Router>
         </div>
