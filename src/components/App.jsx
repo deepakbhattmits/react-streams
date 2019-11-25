7d8268fcf1bcbef8b3b34221248be981bda1b7f8
deepak bhatt <deepakbhattmits@gmail.com>
2019-11-25 23:11:45 +0530

    added new grid attributes for position

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,8 +13,8 @@ import createBrowserHistory from '../history';
 const App = () => {
     const layout = [
         { i: 'a', x: 0, y: 0, w: 4, h: 4, minW: 2, maxW: 4, minH: 3, maxH: 4, isDraggable: false },
-        { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-        { i: 'c', x: 4, y: 0, w: 1, h: 2 }
+        { i: 'b', x: 1, y: 1, w: 3, h: 2, minW: 2, maxW: 4 },
+        { i: 'c', x: 4, y: 1, w: 1, h: 2 }
     ];
     const [selectedDate, setSelectedDate] = useState(new Date());
     const datePicker = useRef();
