7dc3df907db1a559cfb97cf0cffc45bfc5c12877
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-01 08:56:59 +0530

    added new component for slider

---
+++ src/components/streams/SliderComponent.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import { ReactComponent as LeftSVG } from '../../assets/images/icon-left-chevron.svg'
+import { ReactComponent as RightSVG } from '../../assets/images/icon-right-chevron.svg'
+
+const SliderComponent = () => (
+    <>
+        <CarouselProvider
+            naturalSlideWidth={100}
+            naturalSlideHeight={10}
+            totalSlides={3}
+            visibleSlides={2}
+        >
+            <ButtonBack><LeftSVG /></ButtonBack>
+            <Slider>
+                <Slide index={0}>
+                    <div className='slide'>
+                        I am the first Slide.
+                                </div>
+                </Slide>
+                <Slide index={1}>
+                    <div className='slide'>
+                        I am the second Slide.
+                                </div>
+                </Slide>
+                <Slide index={2}>
+                    <div className='slide'>
+                        I am the third Slide.
+                                </div>
+                </Slide>
+            </Slider>
+            <ButtonNext><RightSVG /></ButtonNext>
+        </CarouselProvider>
+
+    </>
+)
+export default SliderComponent
