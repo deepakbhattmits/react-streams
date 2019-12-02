6e54e7cb6862947a8c95c1a6853196438224814a
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-02 22:35:59 +0530

    new changes

--- src/components/streams/StreamShow.jsx
+++ src/components/streams/StreamShow.jsx
@@ -8,51 +8,51 @@ class StreamShow extends Component {
         this.videoRef = createRef();
         this.inputRef = createRef();
     }
-    componentDidMount(){
+    componentDidMount() {
         console.log(this.videoRef);
         const { id } = this.props.match.params;
-        this.props.fetchStream(id);   
+        this.props.fetchStream(id);
         this.buildPlayer();
     }
-    componentDidUpdate(){
+    componentDidUpdate() {
         this.buildPlayer();
     }
     buildPlayer() {
         const { id } = this.props.match.params;
-        if( this.player || !this.props.stream ) {
+        if (this.player || !this.props.stream) {
             return;
-        }    
+        }
         this.player = flv.createPlayer({
-            type    : 'flv',
-            url     : `http://localhost:8000/live/${id}.flv`
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
         });
         this.player.attachMediaElement(this.videoRef.current);
         this.player.load();
-    
+
     }
     handleSubmit = () => {
         console.log(`hello how are you ${this.inputRef.current.value} !`);
     }
-    render(){
-        if(!this.props.stream) {
+    render() {
+        if (!this.props.stream) {
             return (
                 <div>Loading...</div>
             );
         }
         const { title, description } = this.props.stream;
-    return (
-        <div> 
-            <video ref={this.videoRef} style={{ width: '100%' }} controls />
-            <h1>{ title }</h1>
-            <h2>{ description }</h2> 
+        return (
+            <div>
+                <video ref={this.videoRef} style={{ width: '100%' }} controls />
+                <h1>{title}</h1>
+                <h2>{description}</h2>
 
-            <input type="text" style={{padding: '10px', borderRadius: '3px'}} ref={this.inputRef}/>
-            <button onClick={this.handleSubmit} >Enter</button>
-        </div>
-    );
+                <input type="text" style={{ padding: '10px', borderRadius: '3px' }} ref={this.inputRef} />
+                <button onClick={this.handleSubmit} >Enter</button>
+            </div>
+        );
     }
 };
-const mapStateToProps = ( state, ownProps ) => { 
+const mapStateToProps = (state, ownProps) => {
     return { stream: state.streams[ownProps.match.params.id] };
 }
-export default connect(mapStateToProps , { fetchStream })(StreamShow);
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
