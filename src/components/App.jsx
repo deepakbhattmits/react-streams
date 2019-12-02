da7b449e78adbd92acc12bfad7ab9f9c52f16267
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-02 22:33:42 +0530

    imported new module

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,6 +9,7 @@ import Header from './Header';
 import createBrowserHistory from '../history';
 import SliderComponent from './streams/SliderComponent';
 import GridLayoutComponent from './streams/GridLayoutComponent';
+import ChartCompoent from '../components/streams/ChartComponent'
 const App = () => {
     const layout = [
         { i: 'a', x: 0, y: 0, w: 5, h: 5 },
@@ -27,6 +28,10 @@ const App = () => {
                         <Route path="/streams/delete/:id" exact component={StreamDelete}></Route>
                         <Route path="/streams/show/:id" exact component={StreamShow}></Route>
                     </Switch>
+                </div>
+                <div className='chart'>
+
+                    <ChartCompoent />
                 </div>
                 <div className='caousel'>
                     <SliderComponent />
