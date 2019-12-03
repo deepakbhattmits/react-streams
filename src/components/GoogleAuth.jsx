1f0c6c0a6e0e51cd5cad3843b1569f98a1bef056
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-03 21:18:55 +0530

    added changes

--- src/components/GoogleAuth.jsx
+++ src/components/GoogleAuth.jsx
@@ -18,8 +18,10 @@ class GoogleAuth extends Component {
     };
     onAuthChange = (isSignedIn) => {
         if (isSignedIn) {
+
             this.props.signIn(this.auth.currentUser.get().getId());
         } else {
+
             this.props.signOut();
         }
     };
