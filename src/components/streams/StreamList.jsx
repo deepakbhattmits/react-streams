68391465384c323db8432fba3b053354aeae022d
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-08 00:03:11 +0530

    remove warning messages.

--- src/components/streams/StreamList.jsx
+++ src/components/streams/StreamList.jsx
@@ -4,86 +4,79 @@ import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
 const StreamList = props => {
-    const [scroll, setScroll] = useState(false);
-    const renderButton = list => {
-        if (list.userId === props.currentUserId) {
-            return (
-                <div className="right floated content">
-                    <Link to={`/streams/edit/${list.id}`} className="btn btn-primary">
-                        Edit
-                    </Link>
-                    <Link to={`/streams/delete/${list.id}`} className="btn btn-danger">
-                        Delete
-                        </Link>
-                </div>
-            );
-        }
+  const [scroll, setScroll] = useState(false);
+  const renderButton = list => {
+    if (list.userId === props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <Link to={`/streams/edit/${list.id}`} className="btn btn-primary">
+            Edit
+          </Link>
+          <Link to={`/streams/delete/${list.id}`} className="btn btn-danger">
+            Delete
+          </Link>
+        </div>
+      );
     }
+  };
 
-    const renderList = () => {
-        return props.streams.map(list => {
-            return (
-                <div className="item" key={list.id}>
-                    {renderButton(list)}
+  const renderList = () => {
+    return props.streams.map(list => {
+      return (
+        <div className="item" key={list.id}>
+          {renderButton(list)}
 
-                    <i className="large middle aligned icon camera" />
+          <i className="large middle aligned icon camera" />
 
-                    <div className="content">
-                        <Link to={`/streams/show/${list.id}`}>
-                            {list.title}
-                            <div className="description">
-                                {list.description}
-                            </div>
-                        </Link>
-                    </div>
-
-                </div>
-            );
-        });
+          <div className="content">
+            <Link to={`/streams/show/${list.id}`}>
+              {list.title}
+              <div className="description">{list.description}</div>
+            </Link>
+          </div>
+        </div>
+      );
+    });
+  };
+  const trackScrolling = () => {
+    setScroll(false);
+  };
+  const handleScroll = e => {
+    const bottom = e.target.scrollTop;
+    // console.log(bottom)
+    if (bottom > 0) {
+      setScroll(true);
+    } else {
+      setScroll(false);
     }
-    const trackScrolling = () => {
-        setScroll(false)
+  };
+  useEffect(() => {
+    if (!props.streams.length) {
+      props.fetchStreams();
     }
-    const handleScroll = (e) => {
-        const bottom = e.target.scrollTop;
-        // console.log(bottom)
-        if (bottom > 0) {
-            setScroll(true)
-        } else {
-            setScroll(false)
-        }
-    }
-    useEffect(() => {
-        if (!props.streams.length) {
-            props.fetchStreams();
-        }
-        document.addEventListener('scroll', trackScrolling);
-        return () => {
-            document.removeEventListener('scroll', trackScrolling);
-        }
-    }, [props])
-
-    return (
-        <div id='divScroll' className='listPage' onScroll={handleScroll} >
-            <div className={`heading  ${scroll ? 'scrolled' : ''}`}>
-                <label className='custom'>Streams</label>
-            </div>
-            <div className="ui celled list">
-                {renderList()}
-
-            </div>
-
-        </div >
-    );
-}
-const mapDispatchToProps = (dispatch) => ({
-    fetchStreams: () => dispatch(fetchStreams()),
-})
-const mapStateToProps = (state) => {
-    return {
-        streams: Object.values(state.streams),
-        currentUserId: state.auth.userId,
-        isSignedIn: state.auth.isSignedIn
+    document.addEventListener('scroll', trackScrolling);
+    return () => {
+      document.removeEventListener('scroll', trackScrolling);
     };
-}
+  }, [props]);
+
+  return (
+    <div id="divScroll" className="listPage" onScroll={handleScroll}>
+      <div className={`heading  ${scroll ? 'scrolled' : ''}`}>
+        <label className="custom">Streams</label>
+      </div>
+      <div className="ui celled list">{renderList()}</div>
+    </div>
+  );
+};
+const mapDispatchToProps = dispatch => ({
+  fetchStreams: () => dispatch(fetchStreams())
+});
+const mapStateToProps = state => {
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
+  };
+};
 export default connect(mapStateToProps, mapDispatchToProps)(StreamList);
