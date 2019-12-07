c0e010676f48ad05e3498ab8b2210ef097da4bc7
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-07 20:00:41 +0530

    added new hooks concept

--- src/components/streams/StreamDelete.jsx
+++ src/components/streams/StreamDelete.jsx
@@ -1,43 +1,39 @@
-import React, { Fragment, Component } from 'react';
+import React, { useEffect } from 'react';
 import Modal from '../Modal';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchStream, deleteStream } from '../../actions';
 import createBrowserHistory from '../../history';
-class StreamDelete extends Component {
-    componentDidMount() {
-
-
-        this.props.fetchStream(this.props.match.params.id);
-    }
-    renderActions() {
-        const { id } = this.props.match.params;
+const StreamDelete = props => {
+    useEffect(() => {
+        props.fetchStream(props.match.params.id);
+    }, [])
+    const renderActions = () => {
+        const { id } = props.match.params;
         return (
-            <Fragment>
-                <button onClick={() => { this.props.deleteStream(id) }}
+            <>
+                <button onClick={() => { props.deleteStream(id) }}
                     className="ui button negative">
                     Delete !
                 </button>
                 <Link to='/' className="ui button">Cancel</Link>
-            </Fragment>
+            </>
         );
     };
-    renderContent() {
-        if (!this.props.stream) {
+    const renderContent = () => {
+        if (!props.stream) {
             return 'Loading ...';
         }
-        return `Are you sure you want to delete this stream ? ${this.props.stream.title} `;
+        return `Are you sure you want to delete this stream ? ${props.stream.title} `;
     }
-    render() {
-        return (
-            <Modal
-                title="Delete Stream"
-                content={this.renderContent()}
-                actions={this.renderActions()}
-                onDismiss={() => createBrowserHistory.push('/')}
-            />
-        );
-    }
+    return (
+        <Modal
+            title="Delete Stream"
+            content={renderContent()}
+            actions={renderActions()}
+            onDismiss={() => createBrowserHistory.push('/')}
+        />
+    );
 }
 const mapStateToProps = (state, ownProps) => {
     return {
