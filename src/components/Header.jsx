e058bc7d62617aae93d6f733173756762cf2de43
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-07 20:00:24 +0530

    added new hooks concept

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,15 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import GoogleAuth from './GoogleAuth'
-
-
-
+import { connect } from 'react-redux'
 import { ReactComponent as CalenderSVG } from '../assets/images/icon-calendar.svg'
 import DatePicker from "react-datepicker"
 import { Dropdown, Button } from 'react-bootstrap'
-const Header = () => {
+const Header = props => {
     const datePicker = useRef();
     const [selectedDate, setSelectedDate] = useState(new Date());
     const [value, setValue] = useState('--Select--');
+    const [display, setDisplay] = useState(false);
     const onChange = value => {
         setSelectedDate(value)
     }
@@ -23,8 +22,38 @@ const Header = () => {
         console.log(innerText)
         setValue(innerText)
     }
+    const trackScrolling = e => {
+        let windowPageYOffset = window.pageYOffset;
+        console.log('HEADER : ', windowPageYOffset)
+        // const elem = document.getElementById('scroll');
+        // let coords = elem.getBoundingClientRect();
+        // let windowHeight = document.documentElement.clientHeight;
+        // let topVisible = coords.top > 0 && coords.top < windowHeight;
+        // console.log(topVisible, windowHeight, coords)
+        if (windowPageYOffset > 2) {
+            setDisplay(true)
+        } else {
+            setDisplay(false)
+        }
+    }
+    const renderCreateButton = () => {
+        if (props.isSignedIn) {
+            return (
+                <div style={{ textAlign: 'right' }}>
+                    <Link className="btn btn-primary" to='/streams/new'>Create Stream</Link>
+                </div>
+            );
+        }
+    }
+    useEffect(() => {
+
+        document.addEventListener('scroll', trackScrolling);
+        return () => {
+            document.removeEventListener('scroll', trackScrolling);
+        }
+    }, [])
     return (
-        <div className="ui secondary menu pointing">
+        <div id='scroll' className={`ui secondary menu pointing ${display ? 'fixed' : ''}`}>
             <Link to='/' className="item">
                 Streamer
             </Link>
@@ -32,6 +61,7 @@ const Header = () => {
                 <Link to='' className="item">All Stream</Link>
 
                 <GoogleAuth />
+                {renderCreateButton()}
 
                 <div className='dropdown'>
                     <Dropdown>
@@ -72,4 +102,9 @@ const Header = () => {
         </div>
     );
 };
-export default Header;
+const mapStateToProps = (state) => {
+    return {
+        isSignedIn: state.auth.isSignedIn
+    };
+}
+export default connect(mapStateToProps, null)(Header);
