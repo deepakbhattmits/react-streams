04dcc9d96c9af1296f877d350cff6da19d9d765d
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-08 08:46:33 +0530

    adde changes

--- src/components/streams/StreamCreate.jsx
+++ src/components/streams/StreamCreate.jsx
@@ -3,20 +3,18 @@ import { connect } from 'react-redux';
 import SteramForm from './StreamForm';
 import { createStream } from '../../actions';
 const StreamCreate = props => {
+  const onSubmit = formValues => {
+    props.createStream(formValues);
+  };
 
-    const onSubmit = formValues => {
-
-        props.createStream(formValues);
-    }
-
-    return (
-        <div>
-            <h3>Create Form </h3>
-            <SteramForm onSubmit={onSubmit} />
-        </div>
-    );
-}
+  return (
+    <div>
+      <h3>Create Form </h3>
+      <SteramForm onSubmit={onSubmit} />
+    </div>
+  );
+};
 const mapDispatchToProps = dispatch => ({
-    createStream: (data) => dispatch(createStream(data))
-})
+  createStream: data => dispatch(createStream(data))
+});
 export default connect(null, mapDispatchToProps)(StreamCreate);
