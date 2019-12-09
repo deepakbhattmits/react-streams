63d2e357e1bbc0ff8db6ce44f3449c060a621df3
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-09 20:25:34 +0530

    added changes

--- src/components/streams/GridLayoutComponent.jsx
+++ src/components/streams/GridLayoutComponent.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import GridLayout from 'react-grid-layout'
+import React from 'react';
+import GridLayout from 'react-grid-layout';
 
 const GridLayoutComponent = ({ layout }) => {
-    return (
-        <>
-            <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
-                <div key="a">a</div>
-                <div key="b">b</div>
-                <div key="c">c</div>
-            </GridLayout>
-        </>
-    )
-}
+  return (
+    <>
+      <GridLayout
+        className='layout'
+        layout={layout}
+        cols={12}
+        rowHeight={30}
+        width={1200}
+      >
+        <div key='a'>A</div>
+        <div key='b'>B</div>
+        <div key='c'>C</div>
+      </GridLayout>
+    </>
+  );
+};
 export default GridLayoutComponent;
