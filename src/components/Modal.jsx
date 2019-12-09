ac9e63c8b8047e96be441529a17b413f2f885694
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-09 20:26:15 +0530

    added changes

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = (props) => {
-    return createPortal(
-        <div onClick={props.onDismiss} className="ui dimmer modals visible active">
-            <div onClick={(e) => { e.stopPropagation() }} className="ui standard modal visible active">
-                <div className="header">{props.title}</div>
-                <div className="content">{props.content}</div>
-                <div className="actions">
-
-                    {props.actions}
-
-                </div>
-            </div>
-        </div>
-        , document.querySelector('#modal')
-    )
-}
+const Modal = props => {
+  return createPortal(
+    <div onClick={props.onDismiss} className='ui dimmer modals visible active'>
+      <div
+        onClick={e => {
+          e.stopPropagation();
+        }}
+        className='ui standard modal visible active'
+      >
+        <div className='header'>{props.title}</div>
+        <div className='content'>{props.content}</div>
+        <div className='actions'>{props.actions}</div>
+      </div>
+    </div>,
+    document.querySelector('#modal')
+  );
+};
 export default Modal;
