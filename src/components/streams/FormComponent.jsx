c3f9c3a5298fadb6379344b21a8dc3e98119d964
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-11 20:41:41 +0530

    added new changes

--- src/components/streams/FormComponent.jsx
+++ src/components/streams/FormComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Form, Button, Col } from 'react-bootstrap';
 const FormComponent = () => {
   const [data, setData] = useState({ data: {} });
