bfc062db28ed7b67843abcc8672a956731f0656f
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-12 23:28:08 +0530

    added filteDemo file

--- src/components/streams/FilterDemo.jsx
+++ src/components/streams/FilterDemo.jsx
@@ -1,87 +1,62 @@
-import React, { Component } from 'react';
-class FilterDemo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: [
-        {
-          text: 'First Item',
-          completed: false
-        },
-        {
-          text: 'Second Item',
-          completed: false
-        },
-        {
-          text: 'Third Item',
-          completed: true
-        }
-      ]
-    };
-    this.handleStatus = this.handleStatus.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+import React, { useState, useEffect } from 'react';
+const initialItems = [
+  { text: 'Apples', group: 'fruits' },
+  { text: 'Broccoli', group: 'veg' },
+  { text: 'Chicken', group: 'non_veg' },
+  { text: 'Bacon', group: 'veg' },
+  { text: 'Eggs', group: 'non_veg' },
+  { text: 'Salmon', group: 'veg' },
+  { text: 'Bananas', group: 'fruits' },
+  { text: 'Beer', group: 'bev' },
+  { text: 'Wine', group: 'bev' },
+  { text: 'Yogurt', group: 'veg' }
+];
+const FilteredList = () => {
+  const [list, setList] = useState(initialItems);
+  const [items, setItems] = useState([]);
+  const filterList = event => {
+    var updatedList = list;
+    if (event.target.value.toLowerCase() !== 'all') {
+      updatedList = updatedList.filter(function(item) {
+        return (
+          item.group.toLowerCase().search(event.target.value.toLowerCase()) !==
+          -1
+        );
+      });
+    }
+    setItems(updatedList);
+  };
+  useEffect(() => {
+    setItems(initialItems);
+  }, []);
+  return (
+    <div className='filter-list'>
+      <input type='text' placeholder='Search' onChange={filterList} />
+      <button value='fruits' type='submit' onClick={filterList}>
+        fruits
+      </button>
+      <button value='veg' type='submit' onClick={filterList}>
+        veg
+      </button>
+      <button value='non_veg' type='submit' onClick={filterList}>
+        nonveg
+      </button>
+      <button value='all' type='submit' onClick={filterList}>
+        all
+      </button>
+      <List items={items} />
+    </div>
+  );
+};
 
-  handleSubmit(event) {
-    event.preventDefault();
-    const newItem = {
-      text: this.refs.new.value,
-      completed: false
-    };
-    this.setState({
-      list: this.state.list.concat([newItem])
-    });
-    this.refs.new.value = '';
-  }
+const List = props => {
+  return (
+    <ul>
+      {props.items.map(function(item, i) {
+        return <li key={i}>{item.text}</li>;
+      })}
+    </ul>
+  );
+};
 
-  handleStatus(event) {
-    const itemText = this.state.list[event.target.value].text;
-    const itemStatus = this.state.list[event.target.value].completed;
-    const newItem = {
-      text: itemText,
-      completed: itemStatus ? false : true
-    };
-    const list = this.state.list;
-    list[event.target.value] = newItem;
-    this.setState({
-      list
-    });
-  }
-
-  render() {
-    const Item = this.state.list.map((item, index) => {
-      return (
-        <li
-          onClick={this.handleStatus}
-          className={item.completed ? 'done' : 'default'}
-          value={index}
-          status={item.completed}
-        >
-          {item.text}
-        </li>
-      );
-    });
-
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <input type='text' ref='new'></input>
-          <button type='submit'>Add</button>
-        </form>
-        <ul>{Item}</ul>
-        <div>
-          <button value='all' type='submit'>
-            All
-          </button>
-          <button value='completed' type='submit'>
-            Completed
-          </button>
-          <button value='pending' type='submit'>
-            Pending
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
-export default FilterDemo;
+export default FilteredList;
