b7219b039f1e81c41cf19c9b2347009e408c0970
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-12 21:14:18 +0530

    added changes

--- src/components/streams/ChartComponent.jsx
+++ src/components/streams/ChartComponent.jsx
@@ -8,7 +8,7 @@ const ChartComponent = () => {
     datasets: [
       {
         label: 'apple',
-        data: [11, 18, 2, 16, 5, 2, 6],
+        data: [11, 18, 2, 16, 5, 2, 2],
         backgroundColor: 'rgba(153,255,51,0.6)',
         borderColor: ['rgba(153,255,51,1)'],
         borderWidth: 5
