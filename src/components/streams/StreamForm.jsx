7ffd3ba31930566802ff57691a4f2224f38dfa5f
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-14 11:38:32 +0530

    change stream form

--- src/components/streams/StreamForm.jsx
+++ src/components/streams/StreamForm.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
-class StreamForm extends Component {
-  renderError = ({ touched, error }) => {
+const StreamForm = props => {
+  const renderError = ({ touched, error }) => {
     if (touched && error) {
       return (
         <div className='ui error message'>
@@ -12,45 +12,36 @@ class StreamForm extends Component {
     }
   };
 
-  renderInput = ({ input, label, meta }) => {
+  const renderInput = ({ input, label, meta }) => {
     return (
       <div className={`field ${meta.error && meta.touched ? 'error' : ''}`}>
         <label>{label}</label>
         <input {...input} autoComplete='off' />
-        {this.renderError(meta)}
+        {renderError(meta)}
       </div>
     );
   };
-  onSubmit = formValues => {
-    this.props.onSubmit(formValues);
+  const onSubmit = formValues => {
+    props.onSubmit(formValues);
   };
-  render() {
-    return (
-      <form
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className='ui form error '
-      >
-        <div className='two fields'>
-          <div className='field'>
-            <Field
-              name='title'
-              component={this.renderInput}
-              label='Enter Title '
-            />
-          </div>
-          <div className='field'>
-            <Field
-              name='description'
-              component={this.renderInput}
-              label='Enter Description '
-            />
-          </div>
+  return (
+    <form onSubmit={props.handleSubmit(onSubmit)} className='ui form error '>
+      <div className='two fields'>
+        <div className='field'>
+          <Field name='title' component={renderInput} label='Enter Title ' />
         </div>
-        <button className='ui primary button'>Submit</button>
-      </form>
-    );
-  }
-}
+        <div className='field'>
+          <Field
+            name='description'
+            component={renderInput}
+            label='Enter Description '
+          />
+        </div>
+      </div>
+      <button className='ui primary button'>Submit</button>
+    </form>
+  );
+};
 const validate = formValues => {
   const errors = {};
 
