a6a9566e9f1dcff94ab902b3326b6d86567a814b
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-19 21:06:43 +0530

    new changes

--- src/components/streams/FilterDemo.jsx
+++ src/components/streams/FilterDemo.jsx
@@ -2,9 +2,9 @@
 
 import React, { useState, useEffect } from 'react';
 const initialItems = [
-  { text: 'Apples', group: 'fruits' },
+  { text: 'Apple', group: 'fruits' },
   { text: 'Papaya', group: 'fruits' },
-  { text: 'Bananas', group: 'fruits' },
+  { text: 'Banana', group: 'fruits' },
   { text: 'Grapes', group: 'fruits' },
   { text: 'Broccoli', group: 'veg' },
   { text: 'Bacon', group: 'veg' },
@@ -12,7 +12,7 @@ const initialItems = [
   { text: 'Yogurt', group: 'veg' },
   { text: 'Chicken', group: 'nonveg' },
   { text: 'Meat', group: 'nonveg' },
-  { text: 'Eggs', group: 'nonveg' },
+  { text: 'Egg', group: 'nonveg' },
   { text: 'Beer', group: 'bev' },
   { text: 'Wine', group: 'bev' }
 ];
