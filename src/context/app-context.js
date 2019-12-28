6bfcdb49f542d628b51e12a0a4fbec185bb3c9e1
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-28 14:37:34 +0530

    refector

--- src/context/app-context.js
+++ src/context/app-context.js
@@ -3,19 +3,19 @@
 import { createContext } from 'react';
 export default createContext({
 	products: [
-		{ id: 1, name: 'Item 1', price: 100 },
-		{ id: 2, name: 'Item 2', price: 102 },
-		{ id: 3, name: 'Item 3', price: 150 },
-		{ id: 4, name: 'Item 4', price: 150 },
-		{ id: 5, name: 'Item 5', price: 150 },
-		{ id: 6, name: 'Item 6', price: 150 },
-		{ id: 7, name: 'Item 7', price: 150 },
-		{ id: 8, name: 'Item 8', price: 150 },
-		{ id: 9, name: 'Item 9', price: 150 },
-		{ id: 10, name: 'Item 10', price: 150 },
-		{ id: 11, name: 'Item 11', price: 150 },
-		{ id: 12, name: 'Item 12', price: 150 },
-		{ id: 13, name: 'Item 13', price: 150 }
+		{ id: 1, name: 'apple', price: 100 },
+		{ id: 2, name: 'iphone pro', price: 102 },
+		{ id: 3, name: 'iphone pro', price: 150 },
+		{ id: 4, name: 'iphone pro', price: 150 },
+		{ id: 5, name: 'apple', price: 150 },
+		{ id: 6, name: 'iphone pro', price: 150 },
+		{ id: 7, name: 'iphone X', price: 150 },
+		{ id: 8, name: 'iphone pro', price: 150 },
+		{ id: 9, name: 'iphone pro', price: 150 },
+		{ id: 10, name: 'Mac Book', price: 150 },
+		{ id: 11, name: 'Mac Book', price: 150 },
+		{ id: 12, name: 'Mac Book', price: 150 },
+		{ id: 13, name: 'Mac Book', price: 150 }
 	],
 	seacrhProduct: (value, product) => {}
 });
