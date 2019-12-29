72494f0550375f2993b61617936395880fc5da20
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-29 21:21:40 +0530

    refector

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createPortal } from 'react-dom';
 
 const Modal = props => {
+  // console.log(props)
   return createPortal(
     <div onClick={props.onDismiss} className='ui dimmer modals visible active'>
       <div
