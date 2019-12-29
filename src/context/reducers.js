759e439f33438c6cf7e7359ade283db77f680439
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-29 09:21:26 +0530

    refector

--- src/context/reducers.js
+++ src/context/reducers.js
@@ -2,22 +2,16 @@
 
 import { streamConstants } from '../constants/type';
 const seacrhProduct = (search, state) => {
-	let filteredProducts = [];
 	console.log('REDUCER', search, 'PRODUCTS : ', state.products);
-	if (search.length > 0) {
-		filteredProducts = state.products.filter(item =>
-			// item.name.toLowerCase().indexOf(search.toLowerCase()) !== -1
-			item.name.toLowerCase().includes(search.toLowerCase())
-		);
-	} else {
-		filteredProducts = state.products;
-	}
+	const filteredProducts = state.products.filter(item => {
+		return item.name.toLowerCase().search(search.toLowerCase()) !== -1;
+	});
 	return { ...state, products: filteredProducts };
 };
 export const streamReducer = (state, action) => {
 	switch (action.type) {
 		case streamConstants.SEARCH_QRY:
-			console.log('matched ', action, state);
+			console.log('new matched :', action, state);
 			return seacrhProduct(action.qry, state);
 		default:
 			return state;
