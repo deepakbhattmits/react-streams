7e237bb5f801a079a5bac409fa98267cf8fbb207
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-29 21:22:05 +0530

    refector

--- src/components/streams/GridLayoutComponent.jsx
+++ src/components/streams/GridLayoutComponent.jsx
@@ -4,21 +4,21 @@ import React from 'react';
 import GridLayout from 'react-grid-layout';
 
 const GridLayoutComponent = ({ layout }) => {
-  // console.log('test : ',layout)
-  return (
-    <>
-      <GridLayout
-        className='custom--layout layout'
-        layout={layout}
-        cols={12}
-        rowHeight={35}
-        width={1200}
-      >
-        <div key='a'>A</div>
-        <div key='b'>B</div>
-        <div key='c'>Cc</div>
-      </GridLayout>
-    </>
-  );
+	// console.log('test layout : ',layout)
+	return (
+		<>
+			<GridLayout
+				className='custom--layout layout'
+				layout={layout}
+				cols={12}
+				rowHeight={35}
+				width={1200}
+			>
+				<div key='a'>A</div>
+				<div key='b'>B</div>
+				<div key='c'>Cc</div>
+			</GridLayout>
+		</>
+	);
 };
 export default GridLayoutComponent;
