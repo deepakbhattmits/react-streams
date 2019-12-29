becab8b177da1fe9df63fb3d6e17f8183b9b2862
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-29 23:07:57 +0530

    refector

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -15,6 +15,21 @@ const GlobalState = props => {
 		{ id: 5, name: 'apple', price: 150 },
 		{ id: 6, name: 'iphone pro', price: 150 },
 		{ id: 7, name: 'iphone X', price: 150 },
+		{ id: 8, name: 'iphone XI', price: 150 },
+		{ id: 9, name: 'iphone pro', price: 150 },
+		{ id: 10, name: 'Mac Book pro', price: 150 },
+		{ id: 11, name: 'Mac Book', price: 150 },
+		{ id: 12, name: 'Mac Book', price: 150 },
+		{ id: 13, name: 'Mac Book', price: 150 }
+	];
+	const filteredProducts = [
+		{ id: 1, name: 'apple', price: 100 },
+		{ id: 2, name: 'iphone pro', price: 102 },
+		{ id: 3, name: 'iphone pro', price: 150 },
+		{ id: 4, name: 'iphone pro', price: 150 },
+		{ id: 5, name: 'apple', price: 150 },
+		{ id: 6, name: 'iphone pro', price: 150 },
+		{ id: 7, name: 'iphone X', price: 150 },
 		{ id: 8, name: 'iphone pro', price: 150 },
 		{ id: 9, name: 'iphone pro', price: 150 },
 		{ id: 10, name: 'Mac Book pro', price: 150 },
@@ -22,8 +37,10 @@ const GlobalState = props => {
 		{ id: 12, name: 'Mac Book', price: 150 },
 		{ id: 13, name: 'Mac Book', price: 150 }
 	];
+
 	const [state, dispatch] = useReducer(streamReducer, {
-		products: products
+		products: products,
+		filteredProducts: filteredProducts
 	});
 	const seacrhProduct = (qry, state) => {
 		console.log('globalState SEARCH : ', qry, state);
@@ -35,7 +52,7 @@ const GlobalState = props => {
 	return (
 		<AppContext.Provider
 			value={{
-				products: state.products,
+				filteredProducts: state.filteredProducts,
 				seacrhProduct: seacrhProduct
 			}}
 		>
