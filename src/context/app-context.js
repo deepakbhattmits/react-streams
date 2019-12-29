368aaba3f82ea4b1c457ea52b99ac82accea0263
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-29 09:21:37 +0530

    refector

--- src/context/app-context.js
+++ src/context/app-context.js
@@ -2,20 +2,20 @@
 
 import { createContext } from 'react';
 export default createContext({
-	products: [
-		{ id: 1, name: 'apple', price: 100 },
-		{ id: 2, name: 'iphone pro', price: 102 },
-		{ id: 3, name: 'iphone pro', price: 150 },
-		{ id: 4, name: 'iphone pro', price: 150 },
-		{ id: 5, name: 'apple', price: 150 },
-		{ id: 6, name: 'iphone pro', price: 150 },
-		{ id: 7, name: 'iphone X', price: 150 },
-		{ id: 8, name: 'iphone pro', price: 150 },
-		{ id: 9, name: 'iphone pro', price: 150 },
-		{ id: 10, name: 'Mac Book', price: 150 },
-		{ id: 11, name: 'Mac Book', price: 150 },
-		{ id: 12, name: 'Mac Book', price: 150 },
-		{ id: 13, name: 'Mac Book', price: 150 }
-	],
-	seacrhProduct: (value, product) => {}
+	// products: [
+	// 	{ id: 1, name: 'apple demo', price: 100 },
+	// 	{ id: 2, name: 'iphone pro', price: 102 },
+	// 	{ id: 3, name: 'iphone pro', price: 150 },
+	// 	{ id: 4, name: 'iphone pro', price: 150 },
+	// 	{ id: 5, name: 'apple', price: 150 },
+	// 	{ id: 6, name: 'iphone pro', price: 150 },
+	// 	{ id: 7, name: 'iphone X', price: 150 },
+	// 	{ id: 8, name: 'iphone pro', price: 150 },
+	// 	{ id: 9, name: 'iphone pro', price: 150 },
+	// 	{ id: 10, name: 'Mac Book', price: 150 },
+	// 	{ id: 11, name: 'Mac Book', price: 150 },
+	// 	{ id: 12, name: 'Mac Book', price: 150 },
+	// 	{ id: 13, name: 'Mac Book', price: 150 }
+	// ],
+	// seacrhProduct: (value, product) => {}
 });
