e0e01fb2e4e897f50bce5255db73e6d823bda455
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-29 21:21:48 +0530

    refector

--- src/components/TableModal.jsx
+++ src/components/TableModal.jsx
@@ -4,36 +4,36 @@ import React from 'react';
 import { createPortal } from 'react-dom';
 
 const TableModal = props => {
-  console.log(props);
-  return createPortal(
-    <div
-      onClick={props.onDismiss}
-      className={`ui dimmer modals transition page ${
-        props.active ? 'visible active' : 'hidden'
-      }`}
-    >
-      <div
-        onClick={e => {
-          e.stopPropagation();
-        }}
-        className={`ui standard test modal transition scrolling ${
-          props.active ? 'visible active' : 'hidden'
-        }`}
-      >
-        <div className='header'>{props.title.toUpperCase()}</div>
-        <div className='content'>
-          {!!props.content && (
-            <ul>
-              <li>{props.content.id}</li>
-              <li>{props.content.name}</li>
-              <li>{props.content.price}</li>
-            </ul>
-          )}
-        </div>
-        {/* <div className='actions'>{props.actions}</div> */}
-      </div>
-    </div>,
-    document.querySelector('#modal')
-  );
+	// console.log(props);
+	return createPortal(
+		<div
+			onClick={props.onDismiss}
+			className={`ui dimmer modals transition page ${
+				props.active ? 'visible active' : 'hidden'
+			}`}
+		>
+			<div
+				onClick={e => {
+					e.stopPropagation();
+				}}
+				className={`ui standard test modal transition scrolling ${
+					props.active ? 'visible active' : 'hidden'
+				}`}
+			>
+				<div className='header'>{props.title.toUpperCase()}</div>
+				<div className='content'>
+					{!!props.content && (
+						<ul>
+							<li>{props.content.id}</li>
+							<li>{props.content.name}</li>
+							<li>{props.content.price}</li>
+						</ul>
+					)}
+				</div>
+				{/* <div className='actions'>{props.actions}</div> */}
+			</div>
+		</div>,
+		document.querySelector('#modal')
+	);
 };
 export default TableModal;
