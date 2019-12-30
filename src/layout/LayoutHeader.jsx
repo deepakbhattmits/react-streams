c0a2b1175493c6a6bbbf1ae6c24df613588e3556
deepak bhatt <deepakbhattmits@gmail.com>
2019-12-30 21:27:42 +0530

    refector

--- src/layout/LayoutHeader.jsx
+++ src/layout/LayoutHeader.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 const LayoutHeader = props => {
+        // console.log(props)
     return (
         <>
            <Header />
