ac8ac4a608c6b6a10a1abe5a3216a9d1caee0051
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-07 22:26:24 +0530

    added new changes

--- src/components/streams/StreamShow.jsx
+++ src/components/streams/StreamShow.jsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import React, { useEffect, useRef, useCallback } from 'react';
 import { connect } from 'react-redux';
 
@@ -5,56 +7,56 @@ import flv from 'flv.js';
 
 import { fetchStream } from '../../actions';
 const StreamShow = props => {
-  // console.log('test');
-  const videoRef = useRef(null);
-  const inputRef = useRef(null);
-  const { id } = props.match.params;
-  const { stream } = props;
-  const buildPlayer = useCallback(() => {
-    const player = flv.createPlayer({
-      type: 'flv',
-      url: `http://localhost:8000/live/${id}.flv`
-    });
-    if (player || !stream) {
-      return;
-    }
-    player.attachMediaElement(videoRef.current);
-    player.load();
-  }, [id, stream]);
-  const handleSubmit = () => {
-    console.log(`hello how are you ${inputRef.current.value} !`);
-  };
-  useEffect(() => {
-    props.fetchStream(id);
-    buildPlayer();
-  }, [props, id, buildPlayer]);
-  const { title, description } = props.stream;
-  if (!props.stream) {
-    return <div>Loading...</div>;
-  }
-  return (
-    <div>
-      <video ref={videoRef} style={{ width: '100%' }} controls />
+	// console.log('test');
+	const videoRef = useRef(null);
+	const inputRef = useRef(null);
+	const { id } = props.match.params;
+	const { stream } = props;
+	const buildPlayer = useCallback(() => {
+		const player = flv.createPlayer({
+			type: 'flv',
+			url: `http://localhost:8000/live/${id}.flv`
+		});
+		if (player || !stream) {
+			return;
+		}
+		player.attachMediaElement(videoRef.current);
+		player.load();
+	}, [id, stream]);
+	const handleSubmit = () => {
+		// console.log(`hello how are you ${inputRef.current.value} !`);
+	};
+	useEffect(() => {
+		props.fetchStream(id);
+		buildPlayer();
+	}, [props, id, buildPlayer]);
+	const { title, description } = props.stream;
+	if (!props.stream) {
+		return <div>Loading...</div>;
+	}
+	return (
+		<div>
+			<video ref={videoRef} style={{ width: '100%' }} controls />
 
-      <h1>{title}</h1>
+			<h1>{title}</h1>
 
-      <h2>{description}</h2>
+			<h2>{description}</h2>
 
-      <input
-        className='ui input'
-        type='text'
-        style={{ padding: '10px', borderRadius: '3px' }}
-        ref={inputRef}
-      />
+			<input
+				className='ui input'
+				type='text'
+				style={{ padding: '10px', borderRadius: '3px' }}
+				ref={inputRef}
+			/>
 
-      <button onClick={handleSubmit}>Enter</button>
-    </div>
-  );
+			<button onClick={handleSubmit}>Enter</button>
+		</div>
+	);
 };
 const mapDispatchToProps = dispatch => ({
-  fetchStream: data => dispatch(fetchStream(data))
+	fetchStream: data => dispatch(fetchStream(data))
 });
 const mapStateToProps = (state, ownProps) => {
-  return { stream: state.streams[ownProps.match.params.id] };
+	return { stream: state.streams[ownProps.match.params.id] };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(StreamShow);
