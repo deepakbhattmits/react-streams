7b98a8e6d9914281cd4e9e43624ac2297749d0a7
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-07 22:26:12 +0530

    added new changes

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -23,19 +23,19 @@ const GlobalState = props => {
 		{ id: 13, name: 'Mac Book', price: 150 }
 	];
 	const filteredProducts = [
-		{ id: 1, name: 'apple', price: 100 },
-		{ id: 2, name: 'iphone pro', price: 102 },
-		{ id: 3, name: 'iphone pro', price: 150 },
-		{ id: 4, name: 'iphone pro', price: 150 },
-		{ id: 5, name: 'apple', price: 150 },
-		{ id: 6, name: 'iphone pro', price: 150 },
-		{ id: 7, name: 'iphone X', price: 150 },
-		{ id: 8, name: 'iphone pro', price: 150 },
-		{ id: 9, name: 'iphone pro', price: 150 },
-		{ id: 10, name: 'Mac Book pro', price: 150 },
-		{ id: 11, name: 'Mac Book', price: 150 },
-		{ id: 12, name: 'Mac Book', price: 150 },
-		{ id: 13, name: 'Mac Book', price: 150 }
+		{ id: 1, name: 'anurag pandey', price: 100 },
+		{ id: 2, name: 'amrita pandey', price: 102 },
+		{ id: 3, name: 'deepak bhatt', price: 150 },
+		{ id: 4, name: 'akhilesh singh', price: 150 },
+		{ id: 5, name: 'bharat kg', price: 150 },
+		{ id: 6, name: 'Mahadev birader', price: 150 },
+		{ id: 7, name: 'dinesh kotni', price: 150 },
+		{ id: 8, name: 'praveen vishnoi', price: 150 },
+		{ id: 9, name: 'vinod singh', price: 150 },
+		{ id: 10, name: 'devesh kumar', price: 150 },
+		{ id: 11, name: 'priyanka ', price: 150 },
+		{ id: 12, name: 'pooja upadhyay', price: 150 },
+		{ id: 13, name: 'lalit mohan upadhyay', price: 150 }
 	];
 
 	const [state, dispatch] = useReducer(streamReducer, {
@@ -54,8 +54,7 @@ const GlobalState = props => {
 			value={{
 				filteredProducts: state.filteredProducts,
 				seacrhProduct: seacrhProduct
-			}}
-		>
+			}}>
 			{props.children}
 		</AppContext.Provider>
 	);
