ad8d64f49f27159adcc9f55caa6fb7c1ffeafad5
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-18 22:35:58 +0530

    added new layout sidebar scrollbar

---
+++ src/layout/LayoutSideBar.jsx
@@ -0,0 +1,32 @@
+/** @format */
+
+import React from 'react';
+import ScrollIndicator from '../reusable/ScrollIndicator';
+const LayoutSideBar = props => {
+	const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+	return (
+		<ScrollIndicator id='sidebar' className='sidebar'>
+			<p>SiderBar</p>
+			<p>SiderBar</p>
+			<p>SiderBar</p>
+			<p>SiderBar</p>
+			<p>SiderBar</p>
+			<p>SiderBar</p>
+			<p>SiderBar</p>
+			<p>SiderBar</p>
+			<p>SiderBar</p>
+			<p>SiderBar</p>
+			<p>SiderBar</p>
+			<p>SiderBar</p>
+			<p>SiderBar</p>
+			<p>SiderBar</p>
+
+			<ul>
+				{arr.map((el, i) => {
+					return <li key={i}>{el}</li>;
+				})}
+			</ul>
+		</ScrollIndicator>
+	);
+};
+export default LayoutSideBar;
