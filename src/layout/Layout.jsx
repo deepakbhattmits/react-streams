02ce6e96142432b482df927c4e3317f1e39a2235
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-18 22:38:49 +0530

    comment semantic style

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,12 +3,14 @@
 import React from 'react';
 import LayoutHeader from './LayoutHeader';
 import LayoutFooter from './LayoutFooter';
+import LayoutSideBar from './LayoutSideBar';
 const Layout = props => {
 	console.log('TEST : ', props);
 	return (
 		<>
 			<LayoutHeader />
-			{props.children}
+			<LayoutSideBar />
+			<main className='main'>{props.children}</main>
 			<LayoutFooter />
 		</>
 	);
