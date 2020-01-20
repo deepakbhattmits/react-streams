8eff1d645d1ea02ae80daa71c186a3f7375304bb
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-20 22:52:10 +0530

    refector

--- src/layout/LayoutSideBar.jsx
+++ src/layout/LayoutSideBar.jsx
@@ -3,9 +3,29 @@
 import React from 'react';
 import ScrollIndicator from '../reusable/ScrollIndicator';
 const LayoutSideBar = props => {
-	const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+	const arr = [
+		1,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16,
+		17,
+		18,
+		19
+	];
 	return (
-		<ScrollIndicator id='sidebar' className='sidebar'>
+		<ScrollIndicator id='sidebar' className='app-sidebar'>
 			<p>SiderBar</p>
 			<p>SiderBar</p>
 			<p>SiderBar</p>
