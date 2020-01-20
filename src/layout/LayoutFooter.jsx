27faefd9d15f169df37776b0216983e1b932b7dc
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-20 22:52:26 +0530

    refector

--- src/layout/LayoutFooter.jsx
+++ src/layout/LayoutFooter.jsx
@@ -3,6 +3,6 @@
 import React from 'react';
 const LayoutFooter = props => {
 	console.log('test : ', props);
-	return <div className='footer'>Footer</div>;
+	return <div className='app-footer'>Footer</div>;
 };
 export default LayoutFooter;
