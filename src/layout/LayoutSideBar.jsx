4f2289d2a7fd5399a9e6672fccd58e6063e493cf
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-21 22:08:51 +0530

    added new changes

--- src/layout/LayoutSideBar.jsx
+++ src/layout/LayoutSideBar.jsx
@@ -39,11 +39,11 @@ const LayoutSideBar = props => {
 			<p>SiderBar</p>
 			<p>SiderBar</p>
 
-			{/* <ul>
+			<ul>
 				{arr.map((el, i) => {
 					return <li key={i}>{el}</li>;
 				})}
-			</ul> */}
+			</ul>
 		</ScrollIndicator>
 	);
 };
