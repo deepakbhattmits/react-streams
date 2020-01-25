2670094c7903a0f466d0edc658457cc2189165a6
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-25 22:23:24 +0530

    refector

--- src/layout/LayoutSideBar.jsx
+++ src/layout/LayoutSideBar.jsx
@@ -1,11 +1,202 @@
 /** @format */
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import ScrollIndicator from '../reusable/ScrollIndicator';
 
-import { Tab, Row, Col, Nav } from 'react-bootstrap';
+import { Tab, Nav } from 'react-bootstrap';
 const LayoutSideBar = props => {
 	const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+	const dataFirst = [
+		{
+			role: 'MIM',
+			counts: {
+				chat: {
+					count: 2,
+					active: false,
+					schedule: false
+				},
+				call: {
+					count: 3,
+					active: false,
+					schedule: false
+				},
+				ticket: {
+					count: 4,
+					active: true,
+					schedule: false
+				},
+				rest: {
+					count: 5,
+					active: false,
+					schedule: false
+				},
+				others: {
+					count: 6,
+					active: false,
+					schedule: false
+				}
+			}
+		},
+		{
+			role: 'Admin',
+			counts: {
+				chat: {
+					count: 2,
+					active: false,
+					schedule: false
+				},
+				call: {
+					count: 3,
+					active: false,
+					schedule: false
+				},
+				ticket: {
+					count: 4,
+					active: false,
+					schedule: true
+				},
+				rest: {
+					count: 5,
+					active: false,
+					schedule: false
+				},
+				other: {
+					count: 6,
+					active: false,
+					schedule: false
+				}
+			}
+		}
+	];
+	const dataSecond = [
+		{
+			role: 'MIM',
+			counts: {
+				chat: {
+					count: 2,
+					active: true,
+					schedule: false
+				},
+				call: {
+					count: 3,
+					active: false,
+					schedule: false
+				},
+				ticket: {
+					count: 4,
+					active: false,
+					schedule: false
+				},
+				rest: {
+					count: 5,
+					active: false,
+					schedule: false
+				},
+				others: {
+					count: 6,
+					active: false,
+					schedule: false
+				}
+			}
+		},
+		{
+			role: 'Admin',
+			counts: {
+				chat: {
+					count: 2,
+					active: false,
+					schedule: true
+				},
+				call: {
+					count: 3,
+					active: false,
+					schedule: true
+				},
+				ticket: {
+					count: 4,
+					active: false,
+					schedule: true
+				},
+				rest: {
+					count: 5,
+					active: false,
+					schedule: false
+				},
+				other: {
+					count: 6,
+					active: false,
+					schedule: false
+				}
+			}
+		}
+	];
+	const renderHeader = () => {
+		console.log('INITIAL : ', dataFirst[0].counts);
+		return Object.keys(dataFirst[0].counts).map((head, i) => {
+			console.log('item label : ', head);
+			return (
+				<div key={i} className='grid-item'>
+					<span className='head'>{head} </span>
+				</div>
+			);
+		});
+	};
+	const renderListFirst = () => {
+		return (
+			!!dataFirst &&
+			dataFirst.map((el, i) => {
+				console.log('RENDER LIST : ', el, el.counts);
+				return (
+					<Fragment key={i}>
+						<div key={i} className='grid-item --role'>
+							<span className='role'>{el.role}</span>
+						</div>
+						{Object.values(el.counts).map((inner, i) => {
+							return (
+								<div
+									key={i}
+									className={`grid-item ${
+										inner.active
+											? 'active'
+											: `${inner.schedule ? 'schedule' : ''}`
+									} `}>
+									<span className='count'>{inner.count}</span>
+								</div>
+							);
+						})}
+					</Fragment>
+				);
+			})
+		);
+	};
+	const renderListSecond = () => {
+		return (
+			!!dataSecond &&
+			dataSecond.map((el, i) => {
+				console.log('RENDER LIST : ', el, el.counts);
+				return (
+					<Fragment key={i}>
+						<div key={i} className='grid-item --role'>
+							<span className='role'>{el.role}</span>
+						</div>
+						{Object.values(el.counts).map((inner, i) => {
+							return (
+								<div
+									key={i}
+									className={`grid-item ${
+										inner.active
+											? 'active'
+											: `${inner.schedule ? 'schedule' : ''}`
+									} `}>
+									<span className='count'>{inner.count}</span>
+								</div>
+							);
+						})}
+					</Fragment>
+				);
+			})
+		);
+	};
 	return (
 		<ScrollIndicator id='sidebar' className='app-sidebar'>
 			<Tab.Container id='left-tabs-example' defaultActiveKey='first'>
@@ -24,8 +215,24 @@ const LayoutSideBar = props => {
 				<Row> */}
 				{/* <Col sm={12}> */}
 				<Tab.Content>
-					<Tab.Pane eventKey='first'>first</Tab.Pane>
-					<Tab.Pane eventKey='second'>second</Tab.Pane>
+					<Tab.Pane eventKey='first'>
+						<div className='grid-container'>
+							<div className='grid-item'>
+								<span className='blank'></span>
+							</div>
+							{renderHeader()}
+							{renderListFirst()}
+						</div>
+					</Tab.Pane>
+					<Tab.Pane eventKey='second'>
+						<div className='grid-container'>
+							<div className='grid-item'>
+								<span className='blank'></span>
+							</div>
+							{renderHeader()}
+							{renderListSecond()}
+						</div>
+					</Tab.Pane>
 				</Tab.Content>
 				{/* </Col>
 				</Row> */}
