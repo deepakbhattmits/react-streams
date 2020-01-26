6c77f1f3cbd4c3600f1e3a901771d97116c9d3f4
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-26 23:23:34 +0530

    refector

--- src/actions/index.js
+++ src/actions/index.js
@@ -24,7 +24,10 @@ export const signOut = () => {
 };
 export const createStream = formValues => async (dispatch, getState) => {
 	const { userId } = getState().auth;
-	const response = await streams.post('/streams', { ...formValues, userId });
+	const response = await streams.post('/streams/add', {
+		...formValues,
+		userId
+	});
 	dispatch({ type: CREATE_STREAM, payload: response.data });
 	createBrowserHistory.push('/'); // programatic navigation
 };
