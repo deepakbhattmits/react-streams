79503a2e84cfe41c5108a0dd3351b39c6befb8a1
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-29 21:05:07 +0530

    refector

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -13,7 +13,7 @@ export default (state = {}, action) => {
 		case FETCH_STREAMS:
 			return { ...state, ..._.mapKeys(action.payload, '_id') };
 		case FETCH_STREAM:
-			return { ...state, [action.payload.id]: action.payload };
+			return { ...state, [action.payload._id]: action.payload };
 		case CREATE_STREAM:
 			return { ...state, [action.payload.id]: action.payload };
 		case EDIT_STREAM:
