37dac7bbe18993240a0f6bb066a73f2ade507149
deepak bhatt <deepakbhattmits@gmail.com>
2020-02-06 22:49:11 +0530

    style

---
+++ src/db/tab.js
@@ -0,0 +1,76 @@
+/** @format */
+
+export const data = [
+	{
+		firstName: 'deepak',
+		lastName: 'bhatt',
+		age: 30,
+		role: 'developer'
+	},
+	{
+		firstName: 'om',
+		lastName: 'bhatt',
+		age: 25,
+		role: 'business'
+	},
+	{
+		firstName: 'puja',
+		lastName: 'upadhyay',
+		age: 24,
+		role: 'hr'
+	},
+	{
+		firstName: 'dinesh',
+		lastName: 'bhatt',
+		age: 20,
+		role: 'developer'
+	},
+	{
+		firstName: 'ram',
+		lastName: 'kumar',
+		age: 30,
+		role: 'hr'
+	},
+	{
+		firstName: 'rahul',
+		lastName: 'sharma',
+		age: 30,
+		role: 'business'
+	},
+	{
+		firstName: 'vikas',
+		lastName: 'singh',
+		age: 34,
+		role: 'developer'
+	},
+	{
+		firstName: 'manjesh',
+		lastName: 'tyagi',
+		age: 30,
+		role: 'business'
+	},
+	{
+		firstName: 'devesh',
+		lastName: 'kumar',
+		age: 30,
+		role: 'developer'
+	},
+	{
+		firstName: 'akhilesh',
+		lastName: 'singh',
+		age: 30,
+		role: 'developer'
+	},
+	{
+		firstName: 'ashu',
+		lastName: 'sharma',
+		age: 30,
+		role: 'developer'
+	},
+	{
+		firstName: 'rana',
+		lastName: 'kumar',
+		age: 30,
+		role: 'developer'
+	}
+];
