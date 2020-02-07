35943eafb9785bbd11b3b0829a3485bf1d8645d5
deepak bhatt <deepakbhattmits@gmail.com>
2020-02-07 22:25:53 +0530

    refector

--- src/components/streams/StreamDelete.jsx
+++ src/components/streams/StreamDelete.jsx
@@ -9,7 +9,7 @@ import createBrowserHistory from '../../history';
 const StreamDelete = props => {
 	const { id } = props.match.params;
 	const { fetchStream } = props;
-	console.log('fetchStream : ', fetchStream);
+	// console.log('fetchStream : ', fetchStream);
 	useEffect(() => {
 		if (!id.length) {
 			fetchStream(id);
