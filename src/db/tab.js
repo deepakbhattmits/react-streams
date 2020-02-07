0a6defee138594c628165128db50713c5390a9d4
deepak bhatt <deepakbhattmits@gmail.com>
2020-02-07 22:24:58 +0530

    refector

--- src/db/tab.js
+++ src/db/tab.js
@@ -2,72 +2,84 @@
 
 export const data = [
 	{
+		id: 1,
 		firstName: 'deepak',
 		lastName: 'bhatt',
 		age: 30,
 		role: 'developer'
 	},
 	{
+		id: 2,
 		firstName: 'om',
 		lastName: 'bhatt',
 		age: 25,
 		role: 'business'
 	},
 	{
+		id: 3,
 		firstName: 'puja',
 		lastName: 'upadhyay',
 		age: 24,
 		role: 'hr'
 	},
 	{
+		id: 5,
 		firstName: 'dinesh',
 		lastName: 'bhatt',
 		age: 20,
 		role: 'developer'
 	},
 	{
+		id: 6,
 		firstName: 'ram',
 		lastName: 'kumar',
 		age: 30,
 		role: 'hr'
 	},
 	{
+		id: 7,
 		firstName: 'rahul',
 		lastName: 'sharma',
 		age: 30,
 		role: 'business'
 	},
 	{
+		id: 8,
 		firstName: 'vikas',
 		lastName: 'singh',
 		age: 34,
 		role: 'developer'
 	},
 	{
+		id: 9,
 		firstName: 'manjesh',
 		lastName: 'tyagi',
 		age: 30,
 		role: 'business'
 	},
 	{
+		id: 10,
 		firstName: 'devesh',
 		lastName: 'kumar',
 		age: 30,
 		role: 'developer'
 	},
 	{
+		id: 11,
 		firstName: 'akhilesh',
 		lastName: 'singh',
-		age: 30,
+		age: 31,
 		role: 'developer'
 	},
 	{
+		id: 12,
 		firstName: 'ashu',
 		lastName: 'sharma',
 		age: 30,
 		role: 'developer'
 	},
 	{
+		id: 13,
 		firstName: 'rana',
 		lastName: 'kumar',
 		age: 30,
