2ca74d3b7629228b68c0112ae4053a65d3301118
deepak bhatt <deepakbhattmits@gmail.com>
2020-03-02 22:37:51 +0530

    refector

---
+++ src/components/streams/MultiHeader.test.js
@@ -0,0 +1,30 @@
+/** @format */
+
+import React from 'react';
+import {
+	render,
+	cleanup,
+	fireEvent,
+	waitForElement
+} from '@testing-library/react';
+import MultiHeader from './MultiHeader';
+afterEach(cleanup);
+// describe('render without error', () => {
+
+// })
+it('1 renders', () => {
+	const { getByTestId } = render(<MultiHeader />);
+	expect(getByTestId('actions').hasChildNodes.length).toBe(0);
+});
+it('2 renders', async () => {
+	const { getByTestId } = render(<MultiHeader />);
+	console.log(
+		'TESTING : ',
+		// getByTestId('select'),
+		'before click : ',
+		getByTestId('actions').hasChildNodes.length
+	);
+	fireEvent.click(getByTestId('select'));
+	const actions = await waitForElement(() => getByTestId('actions'));
+	console.log('after click TESTING : ', actions.hasChildNodes.length);
+});
