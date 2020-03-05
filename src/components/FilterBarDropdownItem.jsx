b56bc2e571fa1f7d022e79fce20bf7744f314cca
deepak bhatt <deepakbhattmits@gmail.com>
2020-03-05 21:29:15 +0530

    refector

---
+++ src/components/FilterBarDropdownItem.jsx
@@ -0,0 +1,17 @@
+/** @format */
+
+import React from 'react';
+
+const FilterBarDropdownItem = ({ item, filters, toggleFilter }) => (
+	<li
+		className={`filter-bar__item${
+			!!filters.find(filter => filter.name === item.name)
+				? ' filter-bar__dropdown--selected'
+				: ''
+		}`}
+		onClick={() => toggleFilter(item)}>
+		{item.name}
+	</li>
+);
+
+export default FilterBarDropdownItem;
