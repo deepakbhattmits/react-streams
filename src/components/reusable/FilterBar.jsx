16a4c86c3ce0c50c82d44cbc2f596cd264d975ef
deepak bhatt <deepakbhattmits@gmail.com>
2020-03-09 22:30:34 +0530

    refector

--- src/components/reusable/FilterBar.jsx
+++ src/components/reusable/FilterBar.jsx
@@ -8,7 +8,7 @@ import FilterBarItem from './FilterBarItem';
  * @prop {Function}  returnFilters   Callback Function for giving the compiled filters back to parent component.
                                      NOTE: Make sure parent component uses `useCallback` hook.
  * @prop {Boolean}   multiple        Determines whether multiple filters can be applied at the same time or not. Default is true.
- * @prop {Object}    defaultFilter   If a filter should be selected by default
+ * @prop {Array}    defaultFilter   If a filter should be selected by default
  * @prop {Boolean}   canDeselect     Whether the filter bar can have no filters active at all.
  */
 const FilterBar = ({
@@ -19,28 +19,34 @@ const FilterBar = ({
 	canDeselect = true // can have no filters applied
 }) => {
 	// source of truth:
-	const [filters, setFilters] = useState(
-		!!defaultFilter ? [defaultFilter] : []
-	);
+	const [filters, setFilters] = useState(!!defaultFilter ? defaultFilter : []);
 
 	// handle whether to add/remove a filter
 	const toggleFilter = item => {
 		// console.log('FILTER ', item);
 		// this FilterBar should allow multiple selections
-		if (multiple) {
-			if (!!filters.find(filter => filter.name === item.name)) {
-				setFilters(filters.filter(filter => filter.name !== item.name));
-			} else {
-				setFilters([...filters, item]);
-			}
-		} else if (!canDeselect) {
-			setFilters([item]);
+		if (
+			!canDeselect &&
+			filters.length === 1 &&
+			!!filters.find(filter => filter.name === item.name)
+		) {
+			return false;
 		} else {
-			// this filter bar should handle only one selection at a time
-			if (filters.find(value => value.name === item.name)) {
-				setFilters([]);
-			} else {
+			if (multiple) {
+				if (!!filters.find(filter => filter.name === item.name)) {
+					setFilters(filters.filter(filter => filter.name !== item.name));
+				} else {
+					setFilters([...filters, item]);
+				}
+			} else if (!canDeselect) {
 				setFilters([item]);
+			} else {
+				// this filter bar should handle only one selection at a time
+				if (filters.find(value => value.name === item.name)) {
+					setFilters([]);
+				} else {
+					setFilters([item]);
+				}
 			}
 		}
 	};
