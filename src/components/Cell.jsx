f491f123c57f2608b5f26636e57792fb8bba43d7
deepak bhatt <deepakbhattmits@gmail.com>
2020-03-16 22:22:40 +0530

    refector

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,13 +1,17 @@
 /** @format */
 
 import React from 'react';
-const Cell = ({ onClick, val, cellNum }) => (
-	<div
-		className='test cell'
-		onClick={() => {
-			onClick(cellNum);
-		}}>
-		{val}
-	</div>
-);
+const Cell = ({ onClick, val, cellNum }) => {
+	console.log('game TEST : ', val.length);
+	return (
+		<div
+			className={`${val.length !== 0 && (val === 'X' ? 'cross' : 'zero')} cell
+			`}
+			onClick={() => {
+				onClick(cellNum);
+			}}>
+			{val}
+		</div>
+	);
+};
 export default Cell;
