a9cafa3478abf98db4dce5f860f8959d7a81c3b1
deepak bhatt <deepakbhattmits@gmail.com>
2020-03-17 19:56:43 +0530

    refector

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -5,7 +5,8 @@ const Cell = ({ onClick, val, cellNum }) => {
 	console.log('game TEST : ', val.length);
 	return (
 		<div
-			className={`${val.length !== 0 && (val === 'X' ? 'cross' : 'zero')} cell
+			className={`${val.length !== 0 &&
+				(val === 'X' ? 'cross disabled' : 'zero disabled')} cell
 			`}
 			onClick={() => {
 				onClick(cellNum);
