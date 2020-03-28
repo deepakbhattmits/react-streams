b5e8c5e23fc05fbd15a78e636684e846bf97a6aa
deepak bhatt <deepakbhattmits@gmail.com>
2020-03-28 18:37:03 +0530

    refector

--- src/components/streams/StreamEdit.jsx
+++ src/components/streams/StreamEdit.jsx
@@ -2,28 +2,32 @@
 
 import React, { useEffect } from 'react';
 import _ from 'lodash';
-import { connect } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { connect, useSelector } from 'react-redux';
 import { fetchStream, editStream } from '../../actions';
 import StreamForm from './StreamForm';
 const StreamEdit = props => {
-	const { id } = props.match.params;
+	console.log(props);
+	const { id } = useParams();
+	const stream = useSelector(state => state.streams[id]);
+	// const { id } = props.match.params;
 	const { fetchStream } = props;
 
-	// console.log('the fetchStream:', fetchStream);
+	console.log('the fetchStream:', fetchStream);
 	useEffect(() => {
 		fetchStream(id);
 	}, [id, fetchStream]);
 	const onSubmit = formValues => {
 		props.editStream(id, formValues);
 	};
-	if (!props.stream) {
+	if (!stream) {
 		return <div>Loading ..</div>;
 	}
 	return (
 		<div>
 			<h3>Edit Form </h3>
 			<StreamForm
-				initialValues={_.pick(props.stream, 'title', 'description')}
+				initialValues={_.pick(stream, 'title', 'description')}
 				onSubmit={onSubmit}
 			/>
 		</div>
@@ -33,7 +37,7 @@ const mapDispatchToProps = disatch => ({
 	fetchStream: data => disatch(fetchStream(data)),
 	editStream: (data, id) => disatch(editStream(data, id))
 });
-const mapStateToProps = (state, ownProps) => {
-	return { stream: state.streams[ownProps.match.params.id] };
-};
-export default connect(mapStateToProps, mapDispatchToProps)(StreamEdit);
+// const mapStateToProps = (state, ownProps) => {
+// 	return { stream: state.streams[ownProps.match.params.id] };
+// };
+export default connect(null, mapDispatchToProps)(StreamEdit);
