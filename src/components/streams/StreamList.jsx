f39ced32db73262719e534359e2c4d16ff6888a9
deepak bhatt <deepakbhattmits@gmail.com>
2020-03-29 12:00:57 +0530

    refector

--- src/components/streams/StreamList.jsx
+++ src/components/streams/StreamList.jsx
@@ -1,96 +1,78 @@
 /** @format */
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import ScrollIndicator from '../../reusable/ScrollIndicator';
 import { fetchStreams } from '../../actions';
-const StreamList = props => {
-	const [scroll, setScroll] = useState(false);
-	const renderButton = list => {
-		// console.log('render button :', list._id, props.currentUserId);
-		if (list.id === props.currentUserId) {
+
+class StreamList extends React.Component {
+	componentDidMount() {
+		this.props.fetchStreams();
+	}
+
+	renderAdmin(stream) {
+		if (stream.userId === this.props.currentUserId) {
 			return (
 				<div className='right floated content'>
-					<Link to={`/streams/edit/${list._id}`} className='btn btn-primary'>
-						Edit
+					<Link className='ui button primary' to={`/streams/edit/${stream.id}`}>
+						EDIT
 					</Link>
-					<Link to={`/streams/delete/${list._id}`} className='btn btn-danger'>
-						Delete
+					<Link
+						className='ui button negative'
+						to={`/streams/delete/${stream.id}`}>
+						DELETE
 					</Link>
 				</div>
 			);
 		}
-	};
+	}
 
-	const renderList = () => {
-		// console.log('renderList : ', props.streams);
-		return props.streams.map(list => {
+	renderList() {
+		return this.props.streams.map(stream => {
 			return (
-				<div className='item' key={list._id}>
-					{renderButton(list)}
-
-					<i className='large middle aligned icon camera' />
-
+				<div className='item' key={stream.id}>
+					{this.renderAdmin(stream)}
+					<i className='large middle aligned icon camera'></i>
 					<div className='content'>
-						<Link to={`/streams/show/${list._id}`}>
-							{list.title}
-							<div className='description'>{list.description}</div>
-						</Link>
+						<div className='header'>
+							<Link to={`/streams/${stream.id}`}>{stream.title}</Link>
+						</div>
+						<div className='description'>{stream.description}</div>
 					</div>
 				</div>
 			);
 		});
-	};
-	const trackScrolling = () => {
-		const elem = document.getElementById('divScroll');
-		let coords = elem.getBoundingClientRect();
-		let windowHeight = document.documentElement.clientHeight;
-		let topVisible = coords.top > 0 && coords.top < windowHeight;
-		console.log(
-			'TEST :',
-			topVisible,
-			windowHeight,
-			coords,
-			'scrolltop : ',
-			elem
-		);
-		console.log('test element visible : ', coords, topVisible);
-		setScroll(false);
-		if (coords.top >= 54) {
-			setScroll(true);
-		}
-	};
+	}
 
-	useEffect(() => {
-		if (!props.streams.length) {
-			props.fetchStreams();
-		}
-		document.addEventListener('scroll', trackScrolling, true);
-		return () => {
-			document.removeEventListener('scroll', trackScrolling, true);
-		};
-	}, [props]);
-
-	return (
-		<ScrollIndicator id='divScroll' className='listPage'>
-			{/* <div className={`heading  ${scroll ? 'scrolled' : ''}`}>
-				<div className={`heading`}>
-					<label className='custom'>Streams</label>
+	renderCreate() {
+		if (this.props.isSignedIn) {
+			return (
+				<div style={{ textAlign: 'right' }}>
+					<Link to='/streams/new'>
+						<button className='ui button primary'>Create Stream</button>
+					</Link>
 				</div>
-			</div> */}
-			<div className='ui celled list'>{renderList()}</div>
-		</ScrollIndicator>
-	);
-};
-const mapDispatchToProps = dispatch => ({
-	fetchStreams: () => dispatch(fetchStreams())
-});
+			);
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>Streams</h2>
+				<div className='ui celled list'>{this.renderList()}</div>
+				{this.renderCreate()}
+			</div>
+		);
+	}
+}
+
 const mapStateToProps = state => {
 	return {
-		streams: Object.values(state.streams),
 		currentUserId: state.auth.userId,
-		isSignedIn: state.auth.isSignedIn
+		isSignedIn: state.auth.isSignedIn,
+		streams: Object.values(state.streams)
 	};
 };
-export default connect(mapStateToProps, mapDispatchToProps)(StreamList);
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
