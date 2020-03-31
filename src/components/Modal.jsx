8306c45a0c68d4d2b28a78d6a5f9cbf50cfbf14d
deepak bhatt <deepakbhattmits@gmail.com>
2020-03-31 13:42:33 +0530

    refector

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,14 +4,20 @@ import React from 'react';
 import { createPortal } from 'react-dom';
 
 const Modal = props => {
-	console.log('test :>', props);
+	// console.log('Modal :>', props);
 	return createPortal(
-		<div onClick={props.onDismiss} className='ui dimmer modals visible active'>
+		<div
+			onClick={props.onDismiss}
+			className={`ui dimmer page ${
+				!!props.title ? 'visible active' : 'hidden'
+			}`}>
 			<div
 				onClick={e => {
 					e.stopPropagation();
 				}}
-				className='ui standard modal visible active'>
+				className={`ui text modal transition scrolling  ${
+					!!props.title ? 'visible active' : 'hidden'
+				}`}>
 				<div className='header'>{props.title}</div>
 				<div className='content'>{props.content}</div>
 				<div className='actions'>{props.actions}</div>
