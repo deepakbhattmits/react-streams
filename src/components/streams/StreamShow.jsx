cec364d0ba8d52c766e1f9fea9f342d843ffbf21
deepak bhatt <deepakbhattmits@gmail.com>
2020-04-01 20:38:35 +0530

    refector

--- src/components/streams/StreamShow.jsx
+++ src/components/streams/StreamShow.jsx
@@ -29,7 +29,7 @@ const StreamShow = props => {
 		buildPlayer();
 	}, [stream, props, id, buildPlayer]);
 	if (!stream) {
-		return <div>Loading...</div>;
+		return <div>Loading....</div>;
 	}
 	return (
 		<div>
