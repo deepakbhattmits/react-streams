fe3e5837705d949652c8af5301609913a673d21b
deepak bhatt <deepakbhattmits@gmail.com>
2020-04-01 20:29:02 +0530

    refector

--- src/components/CalComponent.jsx
+++ src/components/CalComponent.jsx
@@ -46,7 +46,7 @@ const CalComponent = () => {
 		}
 
 		// let newVal = +`${val}${textContent}`;
-		// console.log('TEST : ', newVal);
+		// console.log('CalComponent : ', newVal);
 		// setValue(val);
 	};
 	const renderData = () => {
