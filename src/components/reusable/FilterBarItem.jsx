ed7126668cd74433fe89bba2f062b5e6c33808d8
deepak bhatt <deepakbhattmits@gmail.com>
2020-04-04 16:02:37 +0530

    refector

--- src/components/reusable/FilterBarItem.jsx
+++ src/components/reusable/FilterBarItem.jsx
@@ -5,14 +5,14 @@ import FilterBarDropdownItem from './FilterBarDropdownItem';
 import { ReactComponent as DownArrowSVG } from '../../assets/images/icon-down-chevron.svg';
 
 const FilterBarItem = ({ item, toggleFilter, filters }) => {
-	// console.log('TEST :: ');
+	// console.log('FilterBarItem :: ');
 	const determineParentClass = () =>
 		!!item.children ? ' filter-bar__item--parent' : '';
 	const determineSelectedClass = () =>
-		!!filters.find(filter => filter.name === item.name) ||
+		!!filters.find((filter) => filter.name === item.name) ||
 		(!!item.children &&
-			filters.find(filter =>
-				item.children.find(child => child.name === filter.name)
+			filters.find((filter) =>
+				item.children.find((child) => child.name === filter.name)
 			))
 			? ' filter-bar__item--selected'
 			: '';
@@ -20,7 +20,7 @@ const FilterBarItem = ({ item, toggleFilter, filters }) => {
 	return (
 		<li
 			className={`filter-bar__item${determineParentClass()}${determineSelectedClass()}`}
-			onClick={e => {
+			onClick={(e) => {
 				e.stopPropagation();
 				if (!item.children) {
 					toggleFilter(item);
@@ -30,7 +30,7 @@ const FilterBarItem = ({ item, toggleFilter, filters }) => {
 			{!!item.children && (
 				<>
 					<ul className='filter-bar__dropdown'>
-						{item.children.map(child => {
+						{item.children.map((child) => {
 							return (
 								// <FilterBarItem key={child.name} item={child} toggleFilter={toggleFilter} filters={filters} />
 								<FilterBarDropdownItem
