2da9755ee9d1a489385edb19782c9adf573b6965
deepak bhatt <deepakbhattmits@gmail.com>
2020-04-04 16:02:47 +0530

    refector

--- src/components/reusable/FilterBarDropdownItem.jsx
+++ src/components/reusable/FilterBarDropdownItem.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 
 const FilterBarDropdownItem = ({ item, filters, toggleFilter }) => (
 	<li
-		className={`filter-bar__item${
-			!!filters.find(filter => filter.name === item.name)
+		className={`demo filter-bar__item${
+			!!filters.find((filter) => filter.name === item.name)
 				? ' filter-bar__dropdown--selected'
 				: ''
 		}`}
