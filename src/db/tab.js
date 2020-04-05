b50609fe42436548fe91c2c93c78b200b4d475f5
deepak bhatt <deepakbhattmits@gmail.com>
2020-04-05 18:46:15 +0530

    refector

--- src/db/tab.js
+++ src/db/tab.js
@@ -3,86 +3,86 @@
 export const data = [
 	{
 		id: 1,
-		firstName: 'deepak',
+		firstName: 'mr deepak',
 		lastName: 'bhatt',
 		age: 30,
-		role: 'developer'
+		role: 'developer',
 	},
 	{
 		id: 2,
 		firstName: 'om prakash',
 		lastName: 'bhatt',
 		age: 25,
-		role: 'business'
+		role: 'business',
 	},
 	{
 		id: 3,
 		firstName: 'puja.',
 		lastName: 'upadhyay',
 		age: 24,
-		role: 'hr'
+		role: 'hr',
 	},
 	{
 		id: 5,
 		firstName: 'dinesh',
 		lastName: 'bhatt',
 		age: 20,
-		role: 'developer'
+		role: 'developer',
 	},
 	{
 		id: 6,
 		firstName: 'ram',
 		lastName: 'kumar',
 		age: 30,
-		role: 'hr'
+		role: 'hr',
 	},
 	{
 		id: 7,
 		firstName: 'rahul',
 		lastName: 'sharma',
 		age: 30,
-		role: 'business'
+		role: 'business',
 	},
 	{
 		id: 8,
 		firstName: 'vikas',
 		lastName: 'singh',
 		age: 34,
-		role: 'developer'
+		role: 'developer',
 	},
 	{
 		id: 9,
 		firstName: 'manjesh',
 		lastName: 'tyagi',
 		age: 30,
-		role: 'business'
+		role: 'business',
 	},
 	{
 		id: 10,
 		firstName: 'devesh',
 		lastName: 'kumar',
 		age: 30,
-		role: 'developer'
+		role: 'developer',
 	},
 	{
 		id: 11,
 		firstName: 'akhilesh',
 		lastName: 'singh',
 		age: 31,
-		role: 'developer'
+		role: 'developer',
 	},
 	{
 		id: 12,
 		firstName: 'ashu',
 		lastName: 'sharma',
 		age: 30,
-		role: 'developer'
+		role: 'developer',
 	},
 	{
 		id: 13,
 		firstName: 'rana',
 		lastName: 'kumar',
 		age: 30,
-		role: 'developer'
-	}
+		role: 'developer',
+	},
 ];
