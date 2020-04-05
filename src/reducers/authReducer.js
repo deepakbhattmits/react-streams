c709856fc7e27b89a55e99e5676a71c77ccd28c3
deepak bhatt <deepakbhattmits@gmail.com>
2020-04-05 18:45:30 +0530

    refector

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -3,12 +3,12 @@
 import { SIGN_IN, SIGN_OUT } from '../actions/types';
 const INITIAL_STATE = {
 	isSignedIn: null,
-	userId: null
+	userId: null,
 };
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
 		case SIGN_IN:
-			// console.log('sign in ::',action.payload);
+			// console.log('sign in :',action.payload);
 			return { ...state, isSignedIn: true, userId: action.payload };
 		case SIGN_OUT:
 			return { ...state, isSignedIn: false, userId: null };
