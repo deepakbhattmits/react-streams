62574c42acb098c2eccf1b0dfd969a6008035db1
deepak bhatt <deepakbhattmits@gmail.com>
2020-04-05 18:45:49 +0530

    refector

--- src/layout/LayoutHeader.jsx
+++ src/layout/LayoutHeader.jsx
@@ -2,8 +2,8 @@
 
 import React from 'react';
 import Header from '../components/Header';
-const LayoutHeader = props => {
-	// console.log('LayoutHeader :', props);
+const LayoutHeader = (props) => {
+	// console.log('LayoutHeader >', props);
 	return (
 		<div className='app-header'>
 			<Header />
