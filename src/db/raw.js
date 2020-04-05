200e3cb4fd2cfa2be1361d6a7be1f388f8b5d792
deepak bhatt <deepakbhattmits@gmail.com>
2020-04-05 18:46:24 +0530

    refector

--- src/db/raw.js
+++ src/db/raw.js
@@ -7,7 +7,7 @@ export const data = [
 		name: 'id labore ex et quam laborum',
 		email: 'Eliseo@gardner.biz',
 		body:
-			'laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium'
+			'laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium',
 	},
 	{
 		postId: 'student',
@@ -15,7 +15,7 @@ export const data = [
 		name: 'quo vero reiciendis velit similique earum',
 		email: 'Jayne_Kuhic@sydney.com',
 		body:
-			'est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et'
+			'enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et',
 	},
 	{
 		postId: 'student',
@@ -23,7 +23,7 @@ export const data = [
 		name: 'odio adipisci rerum aut animi',
 		email: 'Nikita@garfield.biz',
 		body:
-			'quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione'
+			'quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione',
 	},
 	{
 		postId: 'student',
@@ -31,7 +31,7 @@ export const data = [
 		name: 'alias odio sit',
 		email: 'Lew@alysha.tv',
 		body:
-			'non et atque\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\nquia voluptas consequuntur itaque dolor\net qui rerum deleniti ut occaecati'
+			'non et atque\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\nquia voluptas consequuntur itaque dolor\net qui rerum deleniti ut occaecati',
 	},
 	{
 		postId: 'student',
@@ -39,7 +39,7 @@ export const data = [
 		name: 'vero eaque aliquid doloribus et culpa',
 		email: 'Hayden@althea.biz',
 		body:
-			'harum non quasi et ratione\ntempore iure ex voluptates in ratione\nharum architecto fugit inventore cupiditate\nvoluptates magni quo et'
+			'harum non quasi et ratione\ntempore iure ex voluptates in ratione\nharum architecto fugit inventore cupiditate\nvoluptates magni quo et',
 	},
 	{
 		postId: 'business',
@@ -47,7 +47,7 @@ export const data = [
 		name: 'et fugit eligendi deleniti quidem qui sint nihil autem',
 		email: 'Presley.Mueller@myrl.com',
 		body:
-			'doloribus at sed quis culpa deserunt consectetur qui praesentium\naccusamus fugiat dicta\nvoluptatem rerum ut voluptate autem\nvoluptatem repellendus aspernatur dolorem in'
+			'doloribus at sed quis culpa deserunt consectetur qui praesentium\naccusamus fugiat dicta\nvoluptatem rerum ut voluptate autem\nvoluptatem repellendus aspernatur dolorem in',
 	},
 	{
 		postId: 'business',
@@ -55,7 +55,7 @@ export const data = [
 		name: 'repellat consequatur praesentium vel minus molestias voluptatum',
 		email: 'Dallas@ole.me',
 		body:
-			'maiores sed dolores similique labore et inventore et\nquasi temporibus esse sunt id et\neos voluptatem aliquam\naliquid ratione corporis molestiae mollitia quia et magnam dolor'
+			'maiores sed dolores similique labore et inventore et\nquasi temporibus esse sunt id et\neos voluptatem aliquam\naliquid ratione corporis molestiae mollitia quia et magnam dolor',
 	},
 	{
 		postId: 'business',
@@ -63,7 +63,7 @@ export const data = [
 		name: 'et omnis dolorem',
 		email: 'Mallory_Kunze@marie.org',
 		body:
-			'ut voluptatem corrupti velit\nad voluptatem maiores\net nisi velit vero accusamus maiores\nvoluptates quia aliquid ullam eaque'
+			'ut voluptatem corrupti velit\nad voluptatem maiores\net nisi velit vero accusamus maiores\nvoluptates quia aliquid ullam eaque',
 	},
 	{
 		postId: 'business',
@@ -71,7 +71,7 @@ export const data = [
 		name: 'provident id voluptas',
 		email: 'Meghan_Littel@rene.us',
 		body:
-			'sapiente assumenda molestiae atque\nadipisci laborum distinctio aperiam et ab ut omnis\net occaecati aspernatur odit sit rem expedita\nquas enim ipsam minus'
+			'sapiente assumenda molestiae atque\nadipisci laborum distinctio aperiam et ab ut omnis\net occaecati aspernatur odit sit rem expedita\nquas enim ipsam minus',
 	},
 	{
 		postId: 'business',
@@ -79,7 +79,7 @@ export const data = [
 		name: 'eaque et deleniti atque tenetur ut quo ut',
 		email: 'Carmen_Keeling@caroline.name',
 		body:
-			'voluptate iusto quis nobis reprehenderit ipsum amet nulla\nquia quas dolores velit et non\naut quia necessitatibus\nnostrum quaerat nulla et accusamus nisi facilis'
+			'voluptate iusto quis nobis reprehenderit ipsum amet nulla\nquia quas dolores velit et non\naut quia necessitatibus\nnostrum quaerat nulla et accusamus nisi facilis',
 	},
 	{
 		postId: 'shopkeeper',
@@ -87,7 +87,7 @@ export const data = [
 		name: 'fugit labore quia mollitia quas deserunt nostrum sunt',
 		email: 'Veronica_Goodwin@timmothy.net',
 		body:
-			'ut dolorum nostrum id quia aut est\nfuga est inventore vel eligendi explicabo quis consectetur\naut occaecati repellat id natus quo est\nut blanditiis quia ut vel ut maiores ea'
+			'ut dolorum nostrum id quia aut est\nfuga est inventore vel eligendi explicabo quis consectetur\naut occaecati repellat id natus quo est\nut blanditiis quia ut vel ut maiores ea',
 	},
 	{
 		postId: 'shopkeeper',
@@ -95,7 +95,7 @@ export const data = [
 		name: 'modi ut eos dolores illum nam dolor',
 		email: 'Oswald.Vandervort@leanne.org',
 		body:
-			'expedita maiores dignissimos facilis\nipsum est rem est fugit velit sequi\neum odio dolores dolor totam\noccaecati ratione eius rem velit'
+			'expedita maiores dignissimos facilis\nipsum est rem est fugit velit sequi\neum odio dolores dolor totam\noccaecati ratione eius rem velit',
 	},
 	{
 		postId: 'shopkeeper',
@@ -103,7 +103,7 @@ export const data = [
 		name: 'aut inventore non pariatur sit vitae voluptatem sapiente',
 		email: 'Kariane@jadyn.tv',
 		body:
-			'fuga eos qui dolor rerum\ninventore corporis exercitationem\ncorporis cupiditate et deserunt recusandae est sed quis culpa\neum maiores corporis et'
+			'fuga eos qui dolor rerum\ninventore corporis exercitationem\ncorporis cupiditate et deserunt recusandae est sed quis culpa\neum maiores corporis et',
 	},
 	{
 		postId: 'shopkeeper',
@@ -111,7 +111,7 @@ export const data = [
 		name: 'et officiis id praesentium hic aut ipsa dolorem repudiandae',
 		email: 'Nathan@solon.io',
 		body:
-			'vel quae voluptas qui exercitationem\nvoluptatibus unde sed\nminima et qui ipsam aspernatur\nexpedita magnam laudantium et et quaerat ut qui dolorum'
+			'vel quae voluptas qui exercitationem\nvoluptatibus unde sed\nminima et qui ipsam aspernatur\nexpedita magnam laudantium et et quaerat ut qui dolorum',
 	},
 	{
 		postId: 'shopkeeper',
@@ -119,7 +119,7 @@ export const data = [
 		name: 'debitis magnam hic odit aut ullam nostrum tenetur',
 		email: 'Maynard.Hodkiewicz@roberta.com',
 		body:
-			'nihil ut voluptates blanditiis autem odio dicta rerum\nquisquam saepe et est\nsunt quasi nemo laudantium deserunt\nmolestias tempora quo quia'
+			'nihil ut voluptates blanditiis autem odio dicta rerum\nquisquam saepe et est\nsunt quasi nemo laudantium deserunt\nmolestias tempora quo quia',
 	},
 	{
 		postId: 'developer',
@@ -127,7 +127,7 @@ export const data = [
 		name: 'perferendis temporibus delectus optio ea eum ratione dolorum',
 		email: 'Christine@ayana.info',
 		body:
-			'iste ut laborum aliquid velit facere itaque\nquo ut soluta dicta voluptate\nerror tempore aut et\nsequi reiciendis dignissimos expedita consequuntur libero sed fugiat facilis'
+			'iste ut laborum aliquid velit facere itaque\nquo ut soluta dicta voluptate\nerror tempore aut et\nsequi reiciendis dignissimos expedita consequuntur libero sed fugiat facilis',
 	},
 	{
 		postId: 'developer',
@@ -135,7 +135,7 @@ export const data = [
 		name: 'eos est animi quis',
 		email: 'Preston_Hudson@blaise.tv',
 		body:
-			'consequatur necessitatibus totam sed sit dolorum\nrecusandae quae odio excepturi voluptatum harum voluptas\nquisquam sit ad eveniet delectus\ndoloribus odio qui non labore'
+			'consequatur necessitatibus totam sed sit dolorum\nrecusandae quae odio excepturi voluptatum harum voluptas\nquisquam sit ad eveniet delectus\ndoloribus odio qui non labore',
 	},
 	{
 		postId: 'developer',
@@ -143,7 +143,7 @@ export const data = [
 		name: 'aut et tenetur ducimus illum aut nulla ab',
 		email: 'Vincenza_Klocko@albertha.name',
 		body:
-			'veritatis voluptates necessitatibus maiores corrupti\nneque et exercitationem amet sit et\nullam velit sit magnam laborum\nmagni ut molestias'
+			'veritatis voluptates necessitatibus maiores corrupti\nneque et exercitationem amet sit et\nullam velit sit magnam laborum\nmagni ut molestias',
 	},
 	{
 		postId: 'developer',
@@ -151,7 +151,7 @@ export const data = [
 		name: 'sed impedit rerum quia et et inventore unde officiis',
 		email: 'Madelynn.Gorczany@darion.biz',
 		body:
-			'doloribus est illo sed minima aperiam\nut dignissimos accusantium tempore atque et aut molestiae\nmagni ut accusamus voluptatem quos ut voluptates\nquisquam porro sed architecto ut'
+			'doloribus est illo sed minima aperiam\nut dignissimos accusantium tempore atque et aut molestiae\nmagni ut accusamus voluptatem quos ut voluptates\nquisquam porro sed architecto ut',
 	},
 	{
 		postId: 'developer',
@@ -159,7 +159,7 @@ export const data = [
 		name: 'molestias expedita iste aliquid voluptates',
 		email: 'Mariana_Orn@preston.org',
 		body:
-			'qui harum consequatur fugiat\net eligendi perferendis at molestiae commodi ducimus\ndoloremque asperiores numquam qui\nut sit dignissimos reprehenderit tempore'
+			'qui harum consequatur fugiat\net eligendi perferendis at molestiae commodi ducimus\ndoloremque asperiores numquam qui\nut sit dignissimos reprehenderit tempore',
 	},
 	{
 		postId: 'collage student',
@@ -167,7 +167,7 @@ export const data = [
 		name: 'aliquid rerum mollitia qui a consectetur eum sed',
 		email: 'Noemie@marques.me',
 		body:
-			'deleniti aut sed molestias explicabo\ncommodi odio ratione nesciunt\nvoluptate doloremque est\nnam autem error delectus'
+			'deleniti aut sed molestias explicabo\ncommodi odio ratione nesciunt\nvoluptate doloremque est\nnam autem error delectus',
 	},
 	{
 		postId: 'collage student',
@@ -175,7 +175,7 @@ export const data = [
 		name: 'porro repellendus aut tempore quis hic',
 		email: 'Khalil@emile.co.uk',
 		body:
-			'qui ipsa animi nostrum praesentium voluptatibus odit\nqui non impedit cum qui nostrum aliquid fuga explicabo\nvoluptatem fugit earum voluptas exercitationem temporibus dignissimos distinctio\nesse inventore reprehenderit quidem ut incidunt nihil necessitatibus rerum'
+			'qui ipsa animi nostrum praesentium voluptatibus odit\nqui non impedit cum qui nostrum aliquid fuga explicabo\nvoluptatem fugit earum voluptas exercitationem temporibus dignissimos distinctio\nesse inventore reprehenderit quidem ut incidunt nihil necessitatibus rerum',
 	},
 	{
 		postId: 'collage student',
@@ -183,7 +183,7 @@ export const data = [
 		name: 'quis tempora quidem nihil iste',
 		email: 'Sophia@arianna.co.uk',
 		body:
-			'voluptates provident repellendus iusto perspiciatis ex fugiat ut\nut dolor nam aliquid et expedita voluptate\nsunt vitae illo rerum in quos\nvel eligendi enim quae fugiat est'
+			'voluptates provident repellendus iusto perspiciatis ex fugiat ut\nut dolor nam aliquid et expedita voluptate\nsunt vitae illo rerum in quos\nvel eligendi enim quae fugiat est',
 	},
 	{
 		postId: 'collage student',
@@ -191,7 +191,7 @@ export const data = [
 		name: 'in tempore eos beatae est',
 		email: 'Jeffery@juwan.us',
 		body:
-			'repudiandae repellat quia\nsequi est dolore explicabo nihil et\net sit et\net praesentium iste atque asperiores tenetur'
+			'repudiandae repellat quia\nsequi est dolore explicabo nihil et\net sit et\net praesentium iste atque asperiores tenetur',
 	},
 	{
 		postId: 'collage student',
@@ -199,7 +199,7 @@ export const data = [
 		name: 'autem ab ea sit alias hic provident sit',
 		email: 'Isaias_Kuhic@jarrett.net',
 		body:
-			'sunt aut quae laboriosam sit ut impedit\nadipisci harum laborum totam deleniti voluptas odit rem ea\nnon iure distinctio ut velit doloribus\net non ex'
+			'sunt aut quae laboriosam sit ut impedit\nadipisci harum laborum totam deleniti voluptas odit rem ea\nnon iure distinctio ut velit doloribus\net non ex',
 	},
 	{
 		postId: 'shop',
@@ -207,7 +207,7 @@ export const data = [
 		name: 'in deleniti sunt provident soluta ratione veniam quam praesentium',
 		email: 'Russel.Parker@kameron.io',
 		body:
-			'incidunt sapiente eaque dolor eos\nad est molestias\nquas sit et nihil exercitationem at cumque ullam\nnihil magnam et'
+			'incidunt sapiente eaque dolor eos\nad est molestias\nquas sit et nihil exercitationem at cumque ullam\nnihil magnam et',
 	},
 	{
 		postId: 'shop',
@@ -215,7 +215,7 @@ export const data = [
 		name: 'doloribus quibusdam molestiae amet illum',
 		email: 'Francesco.Gleason@nella.us',
 		body:
-			'nisi vel quas ut laborum ratione\nrerum magni eum\nunde et voluptatem saepe\nvoluptas corporis modi amet ipsam eos saepe porro'
+			'nisi vel quas ut laborum ratione\nrerum magni eum\nunde et voluptatem saepe\nvoluptas corporis modi amet ipsam eos saepe porro',
 	},
 	{
 		postId: 'shop',
@@ -224,7 +224,7 @@ export const data = [
 			'quo voluptates voluptas nisi veritatis dignissimos dolores ut officiis',
 		email: 'Ronny@rosina.org',
 		body:
-			'voluptatem repellendus quo alias at laudantium\nmollitia quidem esse\ntemporibus consequuntur vitae rerum illum\nid corporis sit id'
+			'voluptatem repellendus quo alias at laudantium\nmollitia quidem esse\ntemporibus consequuntur vitae rerum illum\nid corporis sit id',
 	},
 	{
 		postId: 'shop',
@@ -232,7 +232,7 @@ export const data = [
 		name: 'eum distinctio amet dolor',
 		email: 'Jennings_Pouros@erica.biz',
 		body:
-			'tempora voluptatem est\nmagnam distinctio autem est dolorem\net ipsa molestiae odit rerum itaque corporis nihil nam\neaque rerum error'
+			'tempora voluptatem est\nmagnam distinctio autem est dolorem\net ipsa molestiae odit rerum itaque corporis nihil nam\neaque rerum error',
 	},
 	{
 		postId: 'shop',
@@ -240,7 +240,7 @@ export const data = [
 		name: 'quasi nulla ducimus facilis non voluptas aut',
 		email: 'Lurline@marvin.biz',
 		body:
-			'consequuntur quia voluptate assumenda et\nautem voluptatem reiciendis ipsum animi est provident\nearum aperiam sapiente ad vitae iste\naccusantium aperiam eius qui dolore voluptatem et'
+			'consequuntur quia voluptate assumenda et\nautem voluptatem reiciendis ipsum animi est provident\nearum aperiam sapiente ad vitae iste\naccusantium aperiam eius qui dolore voluptatem et',
 	},
 	{
 		postId: 'market',
@@ -248,7 +248,7 @@ export const data = [
 		name: 'ex velit ut cum eius odio ad placeat.',
 		email: 'Buford@shaylee.biz',
 		body:
-			'quia incidunt ut\naliquid est ut rerum deleniti iure est\nipsum quia ea sint et\nvoluptatem quaerat eaque repudiandae eveniet aut'
+			'quia incidunt ut\naliquid est ut rerum deleniti iure est\nipsum quia ea sint et\nvoluptatem quaerat eaque repudiandae eveniet aut',
 	},
 	{
 		postId: 'market',
@@ -256,7 +256,7 @@ export const data = [
 		name: 'dolorem architecto ut pariatur quae qui suscipit',
 		email: 'Maria@laurel.name',
 		body:
-			'nihil ea itaque libero illo\nofficiis quo quo dicta inventore consequatur voluptas voluptatem\ncorporis sed necessitatibus velit tempore\nrerum velit et temporibus'
+			'nihil ea itaque libero illo\nofficiis quo quo dicta inventore consequatur voluptas voluptatem\ncorporis sed necessitatibus velit tempore\nrerum velit et temporibus',
 	},
 	{
 		postId: 'market',
@@ -264,7 +264,7 @@ export const data = [
 		name: 'voluptatum totam vel voluptate omnis',
 		email: 'Jaeden.Towne@arlene.tv',
 		body:
-			'fugit harum quae vero\nlibero unde tempore\nsoluta eaque culpa sequi quibusdam nulla id\net et necessitatibus'
+			'fugit harum quae vero\nlibero unde tempore\nsoluta eaque culpa sequi quibusdam nulla id\net et necessitatibus',
 	},
 	{
 		postId: 'market',
@@ -272,7 +272,7 @@ export const data = [
 		name: 'omnis nemo sunt ab autem',
 		email: 'Ethelyn.Schneider@emelia.co.uk',
 		body:
-			'omnis temporibus quasi ab omnis\nfacilis et omnis illum quae quasi aut\nminus iure ex rem ut reprehenderit\nin non fugit'
+			'omnis temporibus quasi ab omnis\nfacilis et omnis illum quae quasi aut\nminus iure ex rem ut reprehenderit\nin non fugit',
 	},
 	{
 		postId: 'market',
@@ -281,7 +281,7 @@ export const data = [
 			'repellendus sapiente omnis praesentium aliquam ipsum id molestiae omnis',
 		email: 'Georgianna@florence.io',
 		body:
-			'dolor mollitia quidem facere et\nvel est ut\nut repudiandae est quidem dolorem sed atque\nrem quia aut adipisci sunt'
+			'dolor mollitia quidem facere et\nvel est ut\nut repudiandae est quidem dolorem sed atque\nrem quia aut adipisci sunt',
 	},
 	{
 		postId: 'market-shop',
@@ -289,7 +289,7 @@ export const data = [
 		name: 'sit et quis',
 		email: 'Raheem_Heaney@gretchen.biz',
 		body:
-			'aut vero est\ndolor non aut excepturi dignissimos illo nisi aut quas\naut magni quia nostrum provident magnam quas modi maxime\nvoluptatem et molestiae'
+			'aut vero est\ndolor non aut excepturi dignissimos illo nisi aut quas\naut magni quia nostrum provident magnam quas modi maxime\nvoluptatem et molestiae',
 	},
 	{
 		postId: 'market-shop',
@@ -297,7 +297,7 @@ export const data = [
 		name: 'beatae veniam nemo rerum voluptate quam aspernatur',
 		email: 'Jacky@victoria.net',
 		body:
-			'qui rem amet aut\ncumque maiores earum ut quia sit nam esse qui\niusto aspernatur quis voluptas\ndolorem distinctio ex temporibus rem'
+			'qui rem amet aut\ncumque maiores earum ut quia sit nam esse qui\niusto aspernatur quis voluptas\ndolorem distinctio ex temporibus rem',
 	},
 	{
 		postId: 'market-shop',
@@ -305,7 +305,7 @@ export const data = [
 		name: 'maiores dolores expedita',
 		email: 'Piper@linwood.us',
 		body:
-			'unde voluptatem qui dicta\nvel ad aut eos error consequatur voluptatem\nadipisci doloribus qui est sit aut\nvelit aut et ea ratione eveniet iure fuga'
+			'unde voluptatem qui dicta\nvel ad aut eos error consequatur voluptatem\nadipisci doloribus qui est sit aut\nvelit aut et ea ratione eveniet iure fuga',
 	},
 	{
 		postId: 'market-shop',
@@ -313,7 +313,7 @@ export const data = [
 		name: 'necessitatibus ratione aut ut delectus quae ut',
 		email: 'Gaylord@russell.co',
 		body:
-			'atque consequatur dolorem sunt\nadipisci autem et\nvoluptatibus et quae necessitatibus rerum eaque aperiam nostrum nemo\neligendi sed et beatae et inventore'
+			'atque consequatur dolorem sunt\nadipisci autem et\nvoluptatibus et quae necessitatibus rerum eaque aperiam nostrum nemo\neligendi sed et beatae et inventore',
 	},
 	{
 		postId: 'market-shop',
@@ -321,6 +321,6 @@ export const data = [
 		name: 'non minima omnis deleniti pariatur facere quibusdam..',
 		email: 'Clare.Aufderhar@nicole.ca',
 		body:
-			'quod minus alias quos\nperferendis labore molestias quae ut ut corporis deserunt vitae\net quaerat ut et ullam unde asperiores\ncum voluptatem cumque'
-	}
+			'quod minus alias quos\nperferendis labore molestias quae ut ut corporis deserunt vitae\net quaerat ut et ullam unde asperiores\ncum voluptatem cumque',
+	},
 ];
