75f726b82e530c07eb8d97f58aacf9c80a40f70b
deepak bhatt <deepakbhattmits@gmail.com>
2020-04-06 07:44:42 +0530

    refector

--- src/context/app-context.js
+++ src/context/app-context.js
@@ -9,7 +9,7 @@ export default createContext({
 	// 	{ id: 4, name: 'iphone X', price: 150 },
 	// 	{ id: 5, name: 'apple', price: 150 },
 	// 	{ id: 6, name: 'iphone pro', price: 150 },
-	// 	{ id: 7, name: 'iphone XI', price: 150 },
+	// 	{ id: 7, name: 'iphone XR', price: 150 },
 	// 	{ id: 8, name: 'iphone pro', price: 150 },
 	// 	{ id: 9, name: 'iphone pro', price: 150 },
 	// 	{ id: 10, name: 'Mac Book', price: 150 },
