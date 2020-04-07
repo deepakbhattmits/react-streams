87a11f4a4ee837d80fe7d6d00d1773825f80c687
deepak bhatt <deepakbhattmits@gmail.com>
2020-04-07 22:42:16 +0530

    refecto

--- src/components/streams/FormComponent.jsx
+++ src/components/streams/FormComponent.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Col } from 'react-bootstrap';
 const FormComponent = () => {
-	// console.log('FormComponent :');
+	// console.log('FormComponent >');
 	const [data, setData] = useState({ data: {} });
 	const [errors, setErrors] = useState({});
 	const validation = () => {
