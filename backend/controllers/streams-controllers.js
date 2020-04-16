335981bee185aac8aa17e008e57b4e156c81837d
deepak bhatt <deepakbhattmits@gmail.com>
2020-04-16 08:25:25 +0530

    refactor

--- backend/controllers/streams-controllers.js
+++ backend/controllers/streams-controllers.js
@@ -1,42 +1,19 @@
 /** @format */
-
-const mongoose = require('mongoose');
 const Stream = require('../models/stream.model');
-
-mongoose.connect('mongodb://127.0.0.1:27017/streams', {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
-// const connection = mongoose.connection;
-
-// connection.once('open', function () {
-// 	console.log('MongoDB database connection established successfully');
-// });
-const getStreams = (req, res, next) => {
-	Stream.find((err, streams) => {
-		if (err) {
-			console.log(err);
-		} else {
-			res.json(streams);
-		}
-	});
+const getStreams = async (req, res, next) => {
+	let streams = await Stream.find().exec();
+	res.json(streams);
 };
-const getStreamById = (req, res, next) => {
+const getStreamById = async (req, res, next) => {
 	const id = req.params.id;
-	Stream.findById(id, function (err, stream) {
-		res.json(stream);
-	});
+	let stream = await Stream.findById(id);
+	res.json(stream);
 };
-const addStream = (req, res, next) => {
+const addStream = async (req, res, next) => {
 	const stream = new Stream(req.body);
-	stream
-		.save()
-		.then((stream) => {
-			res.json({ stream: 'added successfully' });
-		})
-		.catch((err) => {
-			res.status(400).send({ stream: 'adding failed' });
-		});
+	let result = await stream.save();
+
+	res.json(result);
 };
 const updateStream = (req, res, next) => {
 	Stream.findById(req.params.id, function (err, stream) {
