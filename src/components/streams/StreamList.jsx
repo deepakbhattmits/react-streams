9f8db48cfb4e0ab8901e2a039984b6e62d48d082
deepak bhatt <deepakbhattmits@gmail.com>
2020-04-28 06:59:40 +0530

    refactor

--- src/components/streams/StreamList.jsx
+++ src/components/streams/StreamList.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
 const StreamList = () => {
-	// console.log('StreamList >');
+	// console.log('StreamList :');
 	const dispatch = useDispatch();
 	const currentUserId = useSelector((state) => state.auth.userId);
 	// const isSignedIn = useSelector(state => state.auth.isSignedIn);// auth token
