8fd8162643ff2651aabf661855ba435a8d6d5d27
deepak bhatt <deepakbhattmits@gmail.com>
2020-04-28 06:58:58 +0530

    refactor

--- src/context/app-context.js
+++ src/context/app-context.js
@@ -10,7 +10,7 @@ export default createContext({
 	// 	{ id: 5, name: 'apple', price: 150 },
 	// 	{ id: 6, name: 'iphone pro', price: 150 },
 	// 	{ id: 7, name: 'iphone X', price: 150 },
-	// 	{ id: 8, name: 'iphone X', price: 150 },
+	// 	{ id: 8, name: 'iphone 8', price: 150 },
 	// 	{ id: 9, name: 'iphone pro', price: 150 },
 	// 	{ id: 10, name: 'Mac Book', price: 150 },
 	// 	{ id: 11, name: 'Mac Book', price: 150 },
