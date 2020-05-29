a2556514d17b20a51cc8a4a5aafe6ba508bf0eef
deepak bhatt <deepakbhattmits@gmail.com>
2020-05-29 19:27:40 +0530

    added new changes

--- src/layout/LayoutHeader.jsx
+++ src/layout/LayoutHeader.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Header from '../components/Header';
 const LayoutHeader = (props) => {
-	// console.log('LayoutHeader >:', props);
+	// console.log('LayoutHeader >>>:', props);
 	return (
 		<div className='app-header'>
 			<Header />
