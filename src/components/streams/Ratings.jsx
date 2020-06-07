b41930c7e639ae51105ad1ebea97caf459809035
deepak bhatt <deepakbhattmits@gmail.com>
2020-06-07 10:47:54 +0530

    added new component

---
+++ src/components/streams/Ratings.jsx
@@ -0,0 +1,32 @@
+/** @format */
+
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+const Ratings = () => {
+	const renderRadio = () => {
+		let arrRadios = [0, 0.25, 0.5, 0.75, 1];
+		const handleChange = (e) => {
+			const { name, value } = e.target;
+			console.log('TEST : ', name, value);
+		};
+		return arrRadios.map((item, index) => {
+			return (
+				<Form.Check
+					onChange={handleChange}
+					key={index}
+					type='radio'
+					name='ticketConductor'
+					value={item}
+					htmlFor={item}
+					label={item === 0 ? 'off' : `${item * 100}%`}
+				/>
+			);
+		});
+	};
+	return (
+		<>
+			<Form className='radio-wrapper'>{renderRadio()}</Form>
+		</>
+	);
+};
+export default Ratings;
