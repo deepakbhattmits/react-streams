a818ea20e9e89791836aa0cfaae653e490d07ab4
deepak bhatt <deepakbhattmits@gmail.com>
2020-06-09 21:55:33 +0530

    refator

--- src/history.js
+++ src/history.js
@@ -1,6 +1,6 @@
 /** @format */
 
-//  import createHistory from 'history/createBrowserHistory'; // history
+//  import createHistory from 'history/createBrowserHistory'; //  history
 import { createBrowserHistory } from 'history';
 
 export default createBrowserHistory();
