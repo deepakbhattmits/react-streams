b8dd128cb332002dcb0f7b33119aa12794a567b4
deepak bhatt <deepakbhattmits@gmail.com>
2020-06-12 19:43:24 +0530

    refactor

--- src/components/streams/WithoutHeader.jsx
+++ src/components/streams/WithoutHeader.jsx
@@ -4,7 +4,7 @@ import React, { useImperativeHandle, forwardRef } from 'react';
 // Import React Table
 import ReactTable from 'react-table-6';
 const WithOutHeader = forwardRef((props, ref) => {
-	// console.log('WithOutHeader ::>');
+	// console.log('WithOutHeader : :>');
 	// The component instance will be extended
 	// with whatever you return from the callback passed
 	// as the second argument
