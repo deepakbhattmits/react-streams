dd176bcd338f5dae523855d200c85380ff4ab4ef
deepak bhatt <deepakbhattmits@gmail.com>
2020-07-05 15:55:05 +0530

    refactor

--- src/db/raw.js
+++ src/db/raw.js
@@ -36,7 +36,7 @@ export const data = [
 	{
 		postId: 'student',
 		id: 5,
-		name: 'vero eaque aliquid doloribus et culpa.',
+		name: 'vero eaque aliquid doloribus et culpa',
 		email: 'Hayden@althea.biz',
 		body:
 			'harum non quasi et ratione\ntempore iure ex voluptates in ratione\nharum architecto fugit inventore cupiditate\nvoluptates magni quo et',
