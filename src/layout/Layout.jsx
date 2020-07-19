9ce0917a7cef5a623a71a69b3ed1e9505cf89e62
deepak bhatt <deepakbhattmits@gmail.com>
2020-07-19 08:54:07 +0530

    refactor

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,14 +4,23 @@ import React from 'react';
 import LayoutHeader from './LayoutHeader';
 import LayoutFooter from './LayoutFooter';
 import LayoutSideBar from './LayoutSideBar';
+import ErrorBoundary from '../reusable/ErrorBoundary';
 const Layout = (props) => {
 	// console.log('Layout  : >  ', props);
 	return (
 		<>
-			<LayoutHeader />
-			<LayoutSideBar />
-			<main className='app-main'>{props.children}</main>
-			<LayoutFooter />
+			<ErrorBoundary>
+				<LayoutHeader />
+			</ErrorBoundary>
+			<ErrorBoundary>
+				<LayoutSideBar />
+			</ErrorBoundary>
+			<ErrorBoundary>
+				<main className='app-main'>{props.children}</main>
+			</ErrorBoundary>
+			<ErrorBoundary>
+				<LayoutFooter error={true} />
+			</ErrorBoundary>
 		</>
 	);
 };
