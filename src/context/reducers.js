61e5eb2e080f21b199277bc659e33e6bf8442641
deepak bhatt <deepakbhattmits@gmail.com>
2020-07-22 07:19:03 +0530

    Refactor

--- src/context/reducers.js
+++ src/context/reducers.js
@@ -17,7 +17,7 @@ const seacrhProduct = (search, state) => {
 export const streamReducer = (state, action) => {
 	switch (action.type) {
 		case streamConstants.SEARCH_QRY:
-			// console.log('streamReducer :  > ', action, state);
+			// console.log('streamReducer :   > ', action, state);
 			return seacrhProduct(action.qry, state);
 		default:
 			return state;
