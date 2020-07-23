5f3269d7baf353044fe6c87ac0629650a79e83f8
deepak bhatt <deepakbhattmits@gmail.com>
2020-07-23 21:40:41 +0530

    reusable

--- src/layout/LayoutHeader.jsx
+++ src/layout/LayoutHeader.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Header from '../components/Header';
 const LayoutHeader = (props) => {
-	// console.log(' LayoutHeader  :   > ', props);
+	// console.log(' LayoutHeade :   > ', props);
 	return (
 		<div className='app-header'>
 			<Header />
