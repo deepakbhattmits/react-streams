6dd028da495c80c090655349dde9a3d44d6607ed
deepak bhatt <deepakbhattmits@gmail.com>
2020-11-12 00:09:37 +0530

    Update PlannerComponent.jsx

--- src/components/streams/PlannerComponent.jsx
+++ src/components/streams/PlannerComponent.jsx
@@ -1,7 +1,7 @@
 /** @format */
 
 import React, { useState } from 'react';
-//TODO: Group By week
+// TODO: Group By week
 const PlannerComponent = () => {
 	const [isActive, setIsActive] = useState([]);
 
