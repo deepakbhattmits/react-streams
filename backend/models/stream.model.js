4697392d73abcbbef274b502b4adec6b0409c2c1
deepak bhatt <deepakbhattmits@gmail.com>
2020-12-10 00:01:35 +0530

    Update stream.model.js

--- backend/models/stream.model.js
+++ backend/models/stream.model.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
-//  model streamSchema
+//   model streamSchema
 const streamSchema = new Schema({
 	id: { type: String },
 	title: { type: String, required: true },
