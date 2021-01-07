4cb43a7bc921f3b9761ddf48274b99241b74540a
deepak bhatt <deepakbhattmits@gmail.com>
2021-01-07 09:42:05 +0530

    Delete history.js

--- src_/history.js
+++
@@ -1,6 +0,0 @@
-/** @format */
-
-//  import createHistory from 'history/createBrowserHistory'; //   history
-import { createBrowserHistory } from 'history';
-
-export default createBrowserHistory();
