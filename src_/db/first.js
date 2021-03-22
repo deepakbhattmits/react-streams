12b9357266d13914ee6174992158cdbb94d94ead
deepak bhatt <deepakbhattmits@gmail.com>
2021-03-22 09:19:26 +0530

    Create first.js

---
+++ src_/db/first.js
@@ -0,0 +1,47 @@
+{
+  "first": [
+    {
+      "price":123,
+      "description": " this was very good movie.",
+      "id": 2
+    },
+    {
+      "price":123,
+      "description": "super cool animal",
+      "id": 3
+    },
+    {
+      "price":120,
+      "description": "this is app ",
+      "userId": "105578102737547165732",
+      "id": 4
+    },
+    {
+      "price":120,
+      "description": "singh",
+      "id": 5
+    },
+    {
+      "price":120,
+      "description": "12",
+      "userId": "116165121182564953607",
+      "id": 6
+    },
+    {
+      "price":120,
+      "description": "movie",
+      "id": 7
+    },
+    {
+      "price":120,
+      "description": "this was very good movie once 3",
+      "id": 8
+    },
+    {
+      "price":120,
+      "description": "awesome movie",
+      "userId": "116165121182564953607",
+      "id": 9
+    }
+  ]
+}
