119a80396f70cc6107a6032b25d294eb2907d6f7
deepak bhatt <deepakbhattmits@gmail.com>
2021-03-22 09:20:08 +0530

    Create Layout.jsx

---
+++ src_/layout/Layout.jsx
@@ -0,0 +1,27 @@
+/** @format */
+
+import React from 'react';
+import LayoutHeader from './LayoutHeader';
+import LayoutFooter from './LayoutFooter';
+import LayoutSideBar from './LayoutSideBar';
+import ErrorBoundary from '../reusable/ErrorBoundary';
+const Layout = (props) => {
+	// console.log('Layout  : > ', props);
+	return (
+		<>
+			<ErrorBoundary>
+				<LayoutHeader />
+			</ErrorBoundary>
+			<ErrorBoundary>
+				<LayoutSideBar />
+			</ErrorBoundary>
+			<ErrorBoundary>
+				<main className='app-main'>{props.children}</main>
+			</ErrorBoundary>
+			<ErrorBoundary>
+				<LayoutFooter error={true} />
+			</ErrorBoundary>
+		</>
+	);
+};
+export default Layout;
