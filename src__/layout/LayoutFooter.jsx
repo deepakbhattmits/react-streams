170969ded8a74d1cb62bf2440da42832279b78e1
deepak bhatt <deepakbhattmits@gmail.com>
2021-03-28 09:57:30 +0530

    Create LayoutFooter.jsx

---
+++ src__/layout/LayoutFooter.jsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import React from 'react';
+const LayoutFooter = ({ error }) => {
+	// console.log('LayoutFooter :  > ', error);
+	// if (error) {
+	// 	throw new Error('Footer: ');
+	// }
+	return <div className='app-footer'>Footer</div>;
+};
+export default LayoutFooter;
