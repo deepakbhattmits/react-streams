8f5d02660a237c50eb96b9e4c0c50f72fc04a1dc
deepakbhattmits <deepakbhattmits@gmail.com>
2022-05-30 10:48:30 +0530

    Create http-error.js

---
+++ backend/models/http-error.js
@@ -0,0 +1,9 @@
+/** @format */
+// error   HttpError
+class HttpError extends Error {
+	constructor(message, errorCode) {
+		super(message);
+		this.errorCode = errorCode;
+	}
+}
+module.exports = HttpError;
