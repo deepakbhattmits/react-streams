af6760cc1904c45ec94ff7aa8136a7cc7696d267
deepakbhattmits <deepakbhattmits@gmail.com>
2022-05-30 10:57:53 +0530

    Create LoadingSpinner.jsx

---
+++ src/components/reusable/LoadingSpinner.jsx
@@ -0,0 +1,14 @@
+/** @format */
+
+const LoadingSpinner = () => (
+  <div className="ui segment">
+    <div className="ui active dimmer">
+      <div className="ui large text loader">Loading...</div>
+    </div>
+    <p></p>
+    <p></p>
+    <p></p>
+  </div>
+);
+
+export default LoadingSpinner;
