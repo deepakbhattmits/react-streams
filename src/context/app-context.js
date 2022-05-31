b5bf95f79234e3dba7622ae7ebb30e0c314de35c
deepakbhattmits <deepakbhattmits@gmail.com>
2022-05-31 06:51:08 +0530

    Create app-context.js

---
+++ src/context/app-context.js
@@ -0,0 +1,21 @@
+/** @format */
+
+import { createContext } from 'react';
+export default createContext({
+	//   products: [
+	// 	{ id: 1, name: 'apple ', price: 100  },
+	// 	{ id: 2, name: 'iphone XR', price: 102 },
+	// 	{ id: 3, name: 'iphone pro', price: 150 },
+	// 	{ id: 4, name: 'iphone X', price: 150 },
+	// 	{ id: 5, name: 'apple', price: 150 },
+	// 	{ id: 6, name: 'iphone pro', price: 150 },
+	// 	{ id: 7, name: 'iphone X', price: 150 },
+	// 	{ id: 8, name: 'iphone 8', price: 150 },
+	// 	{ id: 9, name: 'iphone pro', price: 150 },
+	// 	{ id: 10, name: 'Mac Book', price: 150 },
+	// 	{ id: 11, name: 'Mac Book', price: 150 },
+	// 	{ id: 12, name: 'Mac Book', price: 150 },
+	// 	{ id: 13, name: 'Mac Book.', price: 152 }
+	// ],
+	// seacrhProduct: (value, product) => {}
+});
