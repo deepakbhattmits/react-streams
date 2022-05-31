4df61c04903d332c8bdd83019d5dae7c49c27d80
deepakbhattmits <deepakbhattmits@gmail.com>
2022-05-31 06:54:43 +0530

    Create tab.js

---
+++ src/db/tab.js
@@ -0,0 +1,88 @@
+/** @format */
+//  data
+export const data = [
+	{
+		id: 1,
+		firstName: ' deepak',
+		lastName: 'bhatt',
+		age: 30,
+		role: 'software developer',
+	},
+	{
+		id: 2,
+		firstName: 'om prakash',
+		lastName: 'bhatt',
+		age: 25,
+		role: 'business developer',
+	},
+	{
+		id: 3,
+		firstName: 'puja',
+		lastName: 'upadhyay',
+		age: 24,
+		role: 'hr',
+	},
+	{
+		id: 5,
+		firstName: 'dinesh',
+		lastName: 'bhatt',
+		age: 20,
+		role: 'clerk',
+	},
+	{
+		id: 6,
+		firstName: 'ram',
+		lastName: 'kumar',
+		age: 30,
+		role: 'hr',
+	},
+	{
+		id: 7,
+		firstName: 'rahul',
+		lastName: 'sharma',
+		age: 30,
+		role: 'business',
+	},
+	{
+		id: 8,
+		firstName: 'vikas',
+		lastName: 'singh',
+		age: 34,
+		role: 'developer',
+	},
+	{
+		id: 9,
+		firstName: 'manjesh',
+		lastName: 'tyagi',
+		age: 30,
+		role: 'business. ',
+	},
+	{
+		id: 10,
+		firstName: 'devesh',
+		lastName: 'kumar',
+		age: 30,
+		role: 'developer',
+	},
+	{
+		id: 11,
+		firstName: 'akhilesh',
+		lastName: 'singh',
+		age: 31,
+		role: 'developer',
+	},
+	{
+		id: 12,
+		firstName: 'ashu',
+		lastName: 'sharma',
+		age: 30,
+		role: 'developer',
+	},
+	{
+		id: 13,
+		firstName: 'rana',
+		lastName: 'kumar',
+		age: 30,
+		role: 'developer',
+	},
+];
