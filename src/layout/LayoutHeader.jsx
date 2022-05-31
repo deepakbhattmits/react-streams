4414e4fb2254f9829aef159c5f36058f8e81e2dc
deepakbhattmits <deepakbhattmits@gmail.com>
2022-05-31 06:55:02 +0530

    Create LayoutHeader.jsx

---
+++ src/layout/LayoutHeader.jsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import React from "react";
+import Header from "../components/Header";
+const LayoutHeader = () => (
+  <div className="app-header">
+    <Header />
+  </div>
+);
+
+export default LayoutHeader;
